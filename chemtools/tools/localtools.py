47fe7c422dbdb8f32f0ca8a8c3cacfb049b65e47
Cristina E. González-Espinoza <crisbeth46@gmail.com>
2015-12-09 18:49:58 -0500

    Change doc of local classes & add mu+,-,0 to linear model.

--- chemtools/tools/localtools.py
+++ chemtools/tools/localtools.py
@@ -21,11 +21,11 @@ class BaseLocalTool(object):
         self._ff_minus = ff_minus
         self._global_instance = global_instance
 
-        # Define central Fukui Functional as the average of FF+ and FF-
+        # Define zero Fukui Functional as the average of FF+ and FF-
         if (self._ff_plus is not None) and (self._ff_minus is not None):
-            self._ff_central = 0.5 * (self._ff_plus + self._ff_minus)
+            self._ff_zero = 0.5 * (self._ff_plus + self._ff_minus)
         else:
-            self._ff_central = None
+            self._ff_zero = None
 
     @property
     def ff_plus(self):
@@ -38,9 +38,9 @@ class BaseLocalTool(object):
         return self._ff_minus
 
     @property
-    def ff_central(self):
+    def ff_zero(self):
         '''Fukui Function from center.'''
-        return self._ff_central
+        return self._ff_zero
 
     @property
     def dual_descriptor(self):
@@ -64,7 +64,7 @@ class BaseLocalTool(object):
             raise ValueError('The global property={0} is not known.'.format(global_prop))
 
         # Check for availability of the type of Fukui Function
-        if ff_type not in ['plus', 'minus', 'central']:
+        if ff_type not in ['plus', 'minus', 'zero']:
             raise ValueError('The attribute ff_{0} is not known.'.format(ff_type))
 
         # Get the global property & the Fukui Function
@@ -84,33 +84,31 @@ class LinearLocalTool(BaseLocalTool):
     Class of Local Conceptual DFT Reactivity Descriptors based on the Linear Energy Model.
     '''
 
+    @doc_inherit(BaseLocalTool)
     def __init__(self, ff_plus=None, ff_minus=None, global_instance=None):
-        '''
-        Parameters
-        ----------
-        ff_plus : np.ndarray
-            Positive Fukui Function.
-        ff_minus : np.ndarray
-            Negative Fukui Function.
-        global_instance :
-            Instance of ``GlobalConceptualTool`` class.
-        '''
         super(self.__class__, self).__init__(ff_plus, ff_minus, global_instance)
+
+    @property
+    def mu_plus(self):
+        '''The local plus chemical potential in the linear model is the positive Fukui function'''
+        return self._ff_plus
+
+    @property
+    def mu_minus(self):
+        '''The local minus chemical potential in the linear model is the negative Fukui function'''
+        return self._ff_minus
+
+    @property
+    def mu_zero(self):
+        '''The local zero chemical potential in the linear model is the zero Fukui function'''
+        return self._ff_zero
 
 
 class QuadraticLocalTool(BaseLocalTool):
     '''
     Class of Local Conceptual DFT Reactivity Descriptors based on the Quadratic Energy Model.
     '''
+
+    @doc_inherit(BaseLocalTool)
     def __init__(self, ff_plus=None, ff_minus=None, global_instance=None):
-        '''
-        Parameters
-        ----------
-        ff_plus : np.ndarray
-            Positive Fukui Function.
-        ff_minus : np.ndarray
-            Negative Fukui Function.
-        global_instance :
-            Instance of ``GlobalConceptualTool`` class.
-        '''
         super(self.__class__, self).__init__(ff_plus, ff_minus, global_instance)
