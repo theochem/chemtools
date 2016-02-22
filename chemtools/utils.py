b4a9160b042221834443d19c6e6a11a636e81f03
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2016-02-23 00:59:25 +0100

    Clean-up CubeGen & complete API documentation, #23.

--- chemtools/utils.py
+++ chemtools/utils.py
@@ -20,11 +20,14 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 #
 # --
+'''The Utility Module.'''
+
+
 import numpy as np
+
 
 __all__ = ['doc_inherit', 'CubeGen']
 
-'''The Utility Module.'''
 
 def doc_inherit(base_class):
     """Docstring inheriting method descriptor
@@ -59,154 +62,179 @@ def doc_inherit(base_class):
 
 
 class CubeGen(object):
-    '''An easy-to-use method to generate cubic grids and cube files)
+    '''
+    Class for generating a cubic grid and writing cube files.
     '''
     def __init__(self, numbers, pseudo_numbers, coordinates, origin, axes, shape):
         '''
         Parameters
         ----------
-        numbers :
-        atom number of molecule.
-        pseudo_numbers :
-        pseudo_numbers of molecule.
-        coordinates : 
-        coordinates of molecule.
-        origin :
-        origin of the cube grid, coordinates of gridpoint[0].
-        axes:
-        vectors along which the cube is constructed.
-        shape:
-        number of points to sample along each direction.
+        numbers : np.ndarray, shape=(M,)
+            Atomic number of `M` atoms in the molecule.
+        pseudo_numbers : np.ndarray, shape=(M,)
+            Pseudo-number of `M` atoms in the molecule.
+        coordinates : np.ndarray, shape=(M, 3)
+            Cartesian coordinates of `M` atoms in the molecule.
+        origin : np.ndarray, shape=(3,)
+            Cartesian coordinates of the cubic grid origin.
+        axes : np.ndarray, shape=(3, 3)
+            The three vectors, stored as rows of axes array,
+            defining the Cartesian coordinate system used to build the
+            cubic grid.
+        shape : np.ndarray, shape=(3,)
+            Number of grid points along `x`, `y`, and `z` axis.
         '''
-
         self._numbers = numbers
         self._pseudo_numbers = pseudo_numbers
         self._coordinates = coordinates
         self._origin = origin
         self._axes = axes
         self._shape = shape
-
-        self._npoints = np.prod(self._shape)
-        self._gridpoints = np.full((self._npoints,3), self._origin)
-        cnt = 0
-        for i in range(0,self._shape[0]):
-            for j in range(0,self._shape[1]):
-                for k in range(0,self._shape[2]):
-                    v = np.array([1.0*i,1.0*j,1.0*k])
-                    self._gridpoints[cnt, :] +=  np.dot(v,self._axes)
-                    cnt += 1
+        #
+        # Make cubic grid
+        #
+        # Number of points along x, y and z axis
+        npoints_x, npoints_y, npoints_z = self._shape
+        # Total number of grid points
+        self._npoints = npoints_x * npoints_y * npoints_z
+        # Make an array to store coordinates of grid points
+        self._gridpoints = np.zeros((self._npoints, 3))
+        # Compute coordinates of grid points relative to the origin
+        self._gridpoints += self._origin
+        count = 0
+        for nx in range(npoints_x):
+            for ny in range(npoints_y):
+                for nz in range(npoints_z):
+                    coordinate = np.dot(np.array([nx, ny, nz]), self._axes)
+                    self._gridpoints[count, :] += coordinate
+                    count += 1
 
     @classmethod
-    def from_molecule(cls, numbers, pseudo_numbers, coordinates, spacing=0.2, treshold=5.0):
+    def from_molecule(cls, numbers, pseudo_numbers, coordinates, spacing=0.2, threshold=5.0):
         '''
+        Initialize ``CubeGen`` class based on the Cartesian coordinates of the molecule.
+
         Parameters
         ----------
-        numbers :
-        atom number of molecule.
-        pseudo_numbers :
-        pseudo_numbers of molecule.
-        coordinates :
-        coordinates of molecule.
-        spacing :
-        size of the step taken in each direction.
-        treshold:
-        extra treshhold in each direction to ensure the plotted function is within the cubefile.
+        numbers : np.ndarray, shape=(M,)
+            Atomic number of `M` atoms in the molecule.
+        pseudo_numbers : np.ndarray, shape=(M,)
+            Pseudo-number of `M` atoms in the molecule.
+        coordinates : np.ndarray, shape=(M, 3)
+            Cartesian coordinates of `M` atoms in the molecule.
+        spacing : float, default=0.2
+            Increment between grid points along `x`, `y` and `z` direction.
+        threshold : float, default=5.0
+            The extension of the cube on each side of the molecule.
         '''
-
-        shape = (np.amax(coordinates, axis=0) - np.amin(coordinates, axis=0) + 2.0*treshold)/spacing
-        midle = (np.amax(coordinates, axis=0) + np.amin(coordinates, axis=0)) / 2.0
-
+        # maximum and minimum value of x, y and z coordinates
+        max_coordinate = np.amax(coordinates, axis=0)
+        min_coordinate = np.amin(coordinates, axis=0)
+        # Compute the required number of points along x, y, and z axis
+        shape = (max_coordinate - min_coordinate + 2.0 * threshold) / spacing
         shape = np.ceil(shape)
-
-        origin = midle - 0.5*shape*spacing
-
         shape = np.array(shape, int)
-
-        axes=np.zeros((3, 3),float)
-        np.fill_diagonal(axes,spacing)
+        # Compute coordinates of the cubic grid origin
+        middle = (max_coordinate + min_coordinate) / 2.0
+        origin = middle - 0.5 * shape * spacing
+        # Compute the unit vectors of the cubic grid's coordinate system
+        axes = np.diag([spacing, spacing, spacing])
 
         return cls(numbers, pseudo_numbers, coordinates, origin, axes, shape)
 
     @classmethod
     def from_cube(cls, filename):
+        '''
+        Initialize ``CubeGen`` class based on the grid specifications of a cube file.
 
-        if filename.endswith('.cube'):
-            with open(filename) as f:
-                numbers, pseudo_numbers, coordinates, origin, axes, shape = _read_cube_header(f)
-        else:
-            raise ValueError('Unknown file format for reading: %s' % filename)
+        Parameters
+        ----------
+        filename : str
+            Cube file name with *.cube extension.
+        '''
+        if not filename.endswith('.cube'):
+            raise ValueError('Arguemnt filename should be a cube file with *.cube extension!')
+
+        # Extract the specifications of the cubic grid from cube file's header
+        numbers, pseudo_numbers, coordinates, origin, axes, shape = cls._read_cube_header(filename)
 
         return cls(numbers, pseudo_numbers, coordinates, origin, axes, shape)
 
     @property
     def numbers(self):
         '''
-        atomic numbers of the molecule.
+        Atomic number of the atoms in the molecule.
         '''
         return self._numbers
 
     @property
     def pseudo_numbers(self):
         '''
-        pseudo_number of the molecule.
+        Pseudo-number of the atoms in the molecule.
         '''
         return self._pseudo_numbers
 
     @property
     def coordinates(self):
         '''
-        coordinates of the molecule.
+        Cartesian coordinates of the atoms in the molecule.
         '''
         return self._coordinates
 
     @property
     def origin(self):
         '''
-        origin of the cube grid, coordinates of gridpoint[0].
+        Cartesian coordinate of the cubic grid origin.
         '''
         return self._origin
 
     @property
     def axes(self):
         '''
-        vectors along which the cube is constructed.
+        The three vectors, stored as rows of axes array, defining the Cartesian
+        coordinate system used to build the cubic grid.
         '''
         return self._axes
 
     @property
     def shape(self):
         '''
-        shape of the cubic grid.
+        Number of grid points along `x`, `y`, and `z` axis.
         '''
         return self._shape
 
     @property
     def npoints(self):
         '''
-        number of moints in the cubic grid.
+        Total number of grid points.
         '''
         return self._npoints
 
     @property
     def gridpoints(self):
         '''
-        xyz coordinates of the gridpoints. The shape of this numpy array is [npoints, 3].
+        Cartesian coordinates of the cubic grid points.
         '''
         return self._gridpoints
 
 
     def dump_cube(self, filename, data):
-        '''Write data to a .cube file.
-
-            Parameters
-            ----------
-           filename :
-           name of the file to be written. This usually has the extension
-           ".cube".
-           data :
-           data for the cubefile.
         '''
-        assert data.size == self._npoints , 'data size ({0}) different from cube size ({1}).'.format(data.size, self._npoints)
+        Write the data evaluated on grid points into a *.cube file.
+
+        Parameters
+        ----------
+        filename : str
+            Cube file name with *.cube extension.
+        data : np.ndarray, shape=(npoints,)
+            An array containing the evaluated scalar property on the grid points.
+        '''
+        if not filename.endswith('.cube'):
+            raise ValueError('Arguemnt filename should be a cube file with `*.cube` extension!')
+        if data.size != self._npoints:
+            raise ValueError('Argument data should have the same size as the grid. {0}!={1}'.format(data.size, self._npoints))
+
+        # Write data into the cube file
         with open(filename, 'w') as f:
             title = 'Cubefile created with HORTON CHEMTOOLS'
             # writing the cube header:
@@ -227,52 +255,61 @@ class CubeGen(object):
             counter = 0
             for value in data.flat:
                 f.write(' % 12.5E' % value)
-                if counter%6 == 5:
+                if counter % 6 == 5:
                     f.write('\n')
                 counter += 1
 
+    @staticmethod
+    def _read_cube_header(filename):
+        '''
+        Return specifications of the cubic grid from the given cube file.
 
-def _read_cube_header(f):
-    # skip the title
-    f.readline()
-    # skip the second line
-    f.readline()
+        Parameters
+        ----------
+        filename : str
+            Cube file name with *.cube extension.
+        '''
+        with open(filename) as f:
+            # skip the title
+            f.readline()
+            # skip the second line
+            f.readline()
 
-    def read_grid_line(line):
-        """Read a grid line from the cube file"""
-        words = line.split()
-        return (
-            int(words[0]),
-            np.array([float(words[1]), float(words[2]), float(words[3])], float)
-            # all coordinates in a cube file are in atomic units
-        )
+            def read_grid_line(line):
+                '''Read a number and (x, y, z) coordinate from the cube file line.'''
+                words = line.split()
+                return (
+                    int(words[0]),
+                    np.array([float(words[1]), float(words[2]), float(words[3])], float)
+                    # all coordinates in a cube file are in atomic units
+                )
 
-    # number of atoms and origin of the grid
-    natom, origin = read_grid_line(f.readline())
-    # numer of grid points in A direction and step vector A, and so on
-    shape0, axis0 = read_grid_line(f.readline())
-    shape1, axis1 = read_grid_line(f.readline())
-    shape2, axis2 = read_grid_line(f.readline())
-    shape = np.array([shape0, shape1, shape2], int)
-    axes = np.array([axis0, axis1, axis2])
+            # number of atoms and origin of the grid
+            natom, origin = read_grid_line(f.readline())
+            # numer of grid points in A direction and step vector A, and so on
+            shape0, axis0 = read_grid_line(f.readline())
+            shape1, axis1 = read_grid_line(f.readline())
+            shape2, axis2 = read_grid_line(f.readline())
+            shape = np.array([shape0, shape1, shape2], int)
+            axes = np.array([axis0, axis1, axis2])
 
-    def read_coordinate_line(line):
-        """Read an atom number and coordinate from the cube file"""
-        words = line.split()
-        return (
-            int(words[0]), float(words[1]),
-            np.array([float(words[2]), float(words[3]), float(words[4])], float)
-            # all coordinates in a cube file are in atomic units
-        )
+            def read_coordinate_line(line):
+                '''Read atomic number and (x, y, z) coordinate from the cube file line.'''
+                words = line.split()
+                return (
+                    int(words[0]), float(words[1]),
+                    np.array([float(words[2]), float(words[3]), float(words[4])], float)
+                    # all coordinates in a cube file are in atomic units
+                )
 
-    numbers = np.zeros(natom, int)
-    pseudo_numbers = np.zeros(natom, float)
-    coordinates = np.zeros((natom, 3), float)
-    for i in xrange(natom):
-        numbers[i], pseudo_numbers[i], coordinates[i] = read_coordinate_line(f.readline())
-        # If the pseudo_number field is zero, we assume that no effective core
-        # potentials were used.
-        if pseudo_numbers[i] == 0.0:
-            pseudo_numbers[i] = numbers[i]
+            numbers = np.zeros(natom, int)
+            pseudo_numbers = np.zeros(natom, float)
+            coordinates = np.zeros((natom, 3), float)
+            for i in xrange(natom):
+                numbers[i], pseudo_numbers[i], coordinates[i] = read_coordinate_line(f.readline())
+                # If the pseudo_number field is zero, we assume that no effective core
+                # potentials were used.
+                if pseudo_numbers[i] == 0.0:
+                    pseudo_numbers[i] = numbers[i]
 
-    return numbers, pseudo_numbers, coordinates, origin, axes, shape
+        return numbers, pseudo_numbers, coordinates, origin, axes, shape
