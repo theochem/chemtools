1a8be2886a9b60bb6b79607c92ca4ed97af75318
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2016-03-02 14:50:34 +0100

    Minial draft of condensed tools, #25.

--- chemtools/tool/condensedtool.py
+++ chemtools/tool/condensedtool.py
@@ -23,8 +23,63 @@
 '''Condensed Conceptual Density Functional Theory (DFT) Reactivity Tools.'''
 
 
-class CondensedTool(object):
+class BaseCondensedTool(object):
     '''
+    Base class of condensed conceptual DFT reactivity descriptors.
     '''
-    def __init__(self):
+    def __init__(self, weights, grid):
+        '''
+        Parameters
+        ----------
+        weights :
+            List of atomic weights
+        grid :
+            Molecular grid
+        '''
+        self._weights = weights
+        self._grid = grid
+
+    @property
+    def weights(self):
+        '''
+        '''
+        return self._weights
+
+    @property
+    def grid(self):
+        '''
+        '''
+        return self._grid
+
+    def condense_atoms(self, local_property):
+        r'''
+        Condense local descriptor :math:`p_{\text{local}}\left(\mathbf{r}\right)` into
+        atomic contribution :math:`P_A` defined as:
+
+        .. math::
+
+           P_A = \int \omega_A\left(\mathbf{r}\right) p_{\text{local}}\left(\mathbf{r}\right) d\mathbf{r}
+
+        Parameters
+        ----------
+        local_property : np.ndarray
+            Local descriptor evaluated on grid.
+        '''
+        pass
+
+    def condese_pairs(self, response):
+        r'''
+        Condense response function :math:`f_{\text{response}}\left(\mathbf{r}, \mathbf{r'}\right)` into
+        atomic pair contribution :math:`P_{AB}` defined as:
+
+        .. math::
+
+           P_{AB} = \int \omega_A\left(\mathbf{r}\right) \omega_B\left(\mathbf{r'}\right)
+                       f_{\text{response}}\left(\mathbf{r}, \mathbf{r'}\right) d\mathbf{r} d\mathbf{r'}
+
+        Parameters
+        ----------
+        response : np.ndarray
+            Response evaluated on grid.
+        '''
         pass
