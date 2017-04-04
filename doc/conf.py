22b98b9e3d332debcdffbeccd23bab15c0ca875d
Derrick <yxt1991@gmail.com>
2017-04-04 16:06:39 -0400

    add theme path to conf.py
    1. add sphinx_rtd path to conf.py
    2. remove "show page source" from the webpage
    3. add exclude pattern in conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -120,7 +120,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build','_themes/*',]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -154,6 +154,8 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes/sphinx_rtd_theme/", ]
+html_show_sourcelink = False
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
