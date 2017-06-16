86ead6f6678cfa7e81abae9c6a8d5fb6cc2a1704
Stijn Fias <stijn.fias@gmail.com>
2017-06-15 21:20:19 -0400

    Fix wrong path to DensityLocalTool in orbitalbased.py

--- chemtools/orbtools/orbitalbased.py
+++ chemtools/orbtools/orbitalbased.py
@@ -24,7 +24,7 @@
 
 
 import numpy as np
-from chemtools.toolbox.densitybased import DensityLocalTool
+from chemtools.denstools.densitybased import DensityLocalTool
 
 __all__ = ['OrbitalLocalTool']
 
