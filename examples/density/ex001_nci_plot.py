51562b0bdd9c181e56ed6baf09c6ea9a70bd46e9
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2018-09-07 12:24:09 +0200

    Update example of NCI water dimer

--- examples/density/ex001_nci_plot.py
+++ examples/density/ex001_nci_plot.py
@@ -4,22 +4,21 @@ EX1: Non-Covalent Interactions (NCI) of water dimer
 ===================================================
 
 1. Build the NCI object for water dimmer using fchk files.
-2. Dump files/scripts for visualizing NCI through VMD.
+2. Dump files/scripts for visualizing NCI through VMD (Visual Molecular Dynamics) script.
 """
 
-from chemtools import NCI
+from chemtools import NCI, context
 
 # 1. Build NCI model using default settings
 
-nci = NCI.from_file('../../data/test/h2o_dimer_pbe_sto3g.fchk')
+nci = NCI.from_file(context.get_fn('test/h2o_dimer_pbe_sto3g.fchk'))
 
 # 2. Dump files/scripts for visualizing NCI
+#    Files generated are h2o_dimer-dens.cube, h2o_dimer-grad.cube, & h2o_dimer.vmd
+#    To visualize the iso-surface, use command: $ vmd -e h2o_dimer.vmd
+
 nci.dump_files('h2o_dimer')
 
-# files generated are h2o_dimer-dens.cube, h2o_dimer-grad.cube, & h2o_dimer.vmd
-# if VMD is setup on your system, you can visualize NCI with the command below, &
-# obtain an image like the one displayed on the website above the script.
-# $ vmd -e h2o_dimer.vmd
 
 # DISCARD BELOW:
 # the code below is for displaying the NCI image on the website, you should remove it
