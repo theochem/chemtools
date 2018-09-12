c0df7097a94822ee54ef43b89e21212fa5f47153
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2018-09-12 23:51:46 +0200

    Disable pylint branch check on toolbox.utils

--- chemtools/toolbox/utils.py
+++ chemtools/toolbox/utils.py
@@ -20,6 +20,7 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 #
 # --
+# pragma pylint: disable=too-many-branches,too-many-statements
 """Utility Functions of Toolbox Module."""
 
 
