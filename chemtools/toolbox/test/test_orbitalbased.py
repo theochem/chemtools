c69509667ffc3781886b7914051ddc4937e5d817
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2018-09-15 21:15:12 +0200

    Merge OrbitalLocalTool tests

--- chemtools/toolbox/test/test_orbitalbased.py
+++ chemtools/toolbox/test/test_orbitalbased.py
@@ -20,7 +20,7 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 #
 # --
-# pragma pylint: disable=invalid-name
+# pragma pylint: disable=invalid-name,bad-whitespace
 """Test chemtools.toolbox.orbitalbased."""
 
 
@@ -42,9 +42,14 @@ def test_orbital_based_raises():
     assert_raises(ValueError, OrbitalLocalTool.from_file, filename, np.array([0.0, 1.0]))
     assert_raises(ValueError, OrbitalLocalTool.from_file, filename, np.array([0.0, 1.0, 0.0]))
     assert_raises(ValueError, OrbitalLocalTool.from_file, filename, np.array([[0, 1], [1, 0.]]))
+    # check spin argument
+    tool = OrbitalLocalTool.from_file(filename, np.array([[0., 0., 0.]]))
+    assert_raises(KeyError, tool.compute_orbital_expression, np.array([9]), spin="error")
+    assert_raises(KeyError, tool.compute_orbital_expression, np.array([9]), spin="alph")
+    assert_raises(KeyError, tool.compute_orbital_expression, np.array([9]), spin="bet")
 
 
-def check_orbital_based(tool, data, index_homo, index_lumo):
+def check_orbital_based_properties(tool, data):
     """Test OrbitalLocalTool against stored data arrays."""
     # check density & gradient
     assert_array_almost_equal(tool.density, data["density"], decimal=6)
@@ -56,61 +61,167 @@ def check_orbital_based(tool, data, index_homo, index_lumo):
     assert_allclose(result, data["ke_thomas_fermi"], rtol=1e-08, atol=1e-08)
     result = tool.kinetic_energy_density
     assert_array_almost_equal(result, data["ke_positive_definite"], decimal=6)
-    # check HOMO & LUMO density
-    result = tool.compute_orbital_expression(index_homo)
-    assert_array_almost_equal(result[:, 0], data["density_homo"], decimal=6)
-    result = tool.compute_orbital_expression(index_lumo)
-    assert_array_almost_equal(result[:, 0], data["density_lumo"], decimal=6)
-    result = tool.compute_orbital_expression([index_homo, index_lumo])
-    assert_array_almost_equal(result[:, 0], data["density_homo"], decimal=6)
-    assert_array_almost_equal(result[:, 1], data["density_lumo"], decimal=6)
-    result = tool.compute_orbital_expression((index_homo, index_lumo))
-    assert_array_almost_equal(result[:, 0], data["density_homo"], decimal=6)
-    assert_array_almost_equal(result[:, 1], data["density_lumo"], decimal=6)
-    result = tool.compute_orbital_expression(np.array([index_homo, index_lumo]))
-    assert_array_almost_equal(result[:, 0], data["density_homo"], decimal=6)
-    assert_array_almost_equal(result[:, 1], data["density_lumo"], decimal=6)
+    # check spin chemical potential at T=25000K
+    test = tool.compute_spin_chemical_potential(25000.0)
+    assert_array_almost_equal(test, data["spin_mu_temp_25000"], decimal=6)
+    # check density at T=25000K
+    dens_temp_a, dens_temp_b = tool.compute_temperature_dependent_density(25000.0)
+    assert_array_almost_equal(dens_temp_a, 0.5 * data["density_temp_25000"], decimal=6)
+    assert_array_almost_equal(dens_temp_b, 0.5 * data["density_temp_25000"], decimal=6)
+    # check local ionization potential
+    assert_array_almost_equal(tool.local_ionization_potential, data["local_ip"], decimal=4)
 
 
 def test_orbital_based_from_file_ch4_uhf_ccpvdz():
     # load data computed with Fortran code
     data = np.load(context.get_fn("test/data_orbitalbased_fortran_ch4_uhf_ccpvdz.npz"))
-    # test from_file initialization
+    # test from_file initialization & check against Fortran code
     tool = OrbitalLocalTool.from_file(context.get_fn("test/ch4_uhf_ccpvdz.fchk"), data["points"])
-    # check against Fortran code
-    check_orbital_based(tool, data, 8, 9)
+    check_orbital_based_properties(tool, data)
 
 
 def test_orbital_based_from_molecule_ch4_uhf_ccpvdz():
     # load data computed with Fortran code
     data = np.load(context.get_fn("test/data_orbitalbased_fortran_ch4_uhf_ccpvdz.npz"))
-    # test from_molecule initialization with exp_beta
+    # test from_molecule initialization with exp_beta & check against Fortran code
     molecule = Molecule.from_file(context.get_fn("test/ch4_uhf_ccpvdz.fchk"))
     tool = OrbitalLocalTool(molecule, data["points"])
-    # check against Fortran code
-    check_orbital_based(tool, data, 8, 9)
-    # test from_molecule initialization without exp_beta
+    check_orbital_based_properties(tool, data)
+    # test from_molecule initialization without exp_beta & check against Fortran code
     del molecule._exp_beta
     molecule = Molecule.from_file(context.get_fn("test/ch4_uhf_ccpvdz.fchk"))
     tool = OrbitalLocalTool(molecule, data["points"])
-    # check against Fortran code
-    check_orbital_based(tool, data, 8, 9)
+    check_orbital_based_properties(tool, data)
 
 
 def test_orbital_based_from_file_ch4_rhf_ccpvdz():
     # load data computed with Fortran code
     data = np.load(context.get_fn("test/data_orbitalbased_fortran_ch4_uhf_ccpvdz.npz"))
-    # test from_file initialization
+    # test from_file initialization & check against Fortran code
     tool = OrbitalLocalTool.from_file(context.get_fn("test/ch4_uhf_ccpvdz.fchk"), data["points"])
-    # check against Fortran code
-    check_orbital_based(tool, data, 8, 9)
+    check_orbital_based_properties(tool, data)
 
 
 def test_orbital_based_from_molecule_ch4_rhf_ccpvdz():
     # load data computed with Fortran code
     data = np.load(context.get_fn("test/data_orbitalbased_fortran_ch4_uhf_ccpvdz.npz"))
-    # test from_file initialization
+    # test from_file initialization & check against Fortran code
     molecule = Molecule.from_file(context.get_fn("test/ch4_rhf_ccpvdz.fchk"))
     tool = OrbitalLocalTool(molecule, data["points"])
-    # check against Fortran code
-    check_orbital_based(tool, data, 8, 9)
+    check_orbital_based_properties(tool, data)
+
+
+def check_orbital_expression(tool, data):
+    """Check OrbitalLocalTool.compute_orbital_expression against stored data array."""
+    result = tool.compute_orbital_expression(5)
+    assert_array_almost_equal(result[:, 0], data["orbital_05"], decimal=6)
+    result = tool.compute_orbital_expression(np.array([5]), spin="beta")
+    assert_array_almost_equal(result[:, 0], data["orbital_05"], decimal=6)
+    result = tool.compute_orbital_expression(6)
+    assert_array_almost_equal(result[:, 0], data["orbital_06"], decimal=6)
+    result = tool.compute_orbital_expression(8)
+    assert_array_almost_equal(result[:, 0], data["orbital_08"], decimal=6)
+    result = tool.compute_orbital_expression(9)
+    assert_array_almost_equal(result[:, 0], data["orbital_09"], decimal=6)
+    result = tool.compute_orbital_expression([8, 9])
+    assert_array_almost_equal(result[:, 0], data["orbital_08"], decimal=6)
+    assert_array_almost_equal(result[:, 1], data["orbital_09"], decimal=6)
+    result = tool.compute_orbital_expression([5, 8, 9], spin="beta")
+    assert_array_almost_equal(result[:, 0], data["orbital_05"], decimal=6)
+    assert_array_almost_equal(result[:, 1], data["orbital_08"], decimal=6)
+    assert_array_almost_equal(result[:, 2], data["orbital_09"], decimal=6)
+    result = tool.compute_orbital_expression((8, 9, 5))
+    assert_array_almost_equal(result[:, 0], data["orbital_08"], decimal=6)
+    assert_array_almost_equal(result[:, 1], data["orbital_09"], decimal=6)
+    assert_array_almost_equal(result[:, 2], data["orbital_05"], decimal=6)
+    result = tool.compute_orbital_expression(np.array([8, 9]))
+    assert_array_almost_equal(result[:, 0], data["orbital_08"], decimal=6)
+    assert_array_almost_equal(result[:, 1], data["orbital_09"], decimal=6)
+    result = tool.compute_orbital_expression(np.array([6, 5]), spin="b")
+    assert_array_almost_equal(result[:, 0], data["orbital_06"], decimal=6)
+    assert_array_almost_equal(result[:, 1], data["orbital_05"], decimal=6)
+    result = tool.compute_orbital_expression(np.array([5, 6, 8, 9]), spin="a")
+    assert_array_almost_equal(result[:, 0], data["orbital_05"], decimal=6)
+    assert_array_almost_equal(result[:, 1], data["orbital_06"], decimal=6)
+    assert_array_almost_equal(result[:, 2], data["orbital_08"], decimal=6)
+    assert_array_almost_equal(result[:, 3], data["orbital_09"], decimal=6)
+
+
+def test_orbital_based_from_file_orbital_expression_ch4_uhf_ccpvdz():
+    # load data computed with Fortran code
+    data = np.load(context.get_fn("test/data_orbitalbased_fortran_ch4_uhf_ccpvdz.npz"))
+    # test from_file initialization & check against Fortran code
+    tool = OrbitalLocalTool.from_file(context.get_fn("test/ch4_uhf_ccpvdz.fchk"), data["points"])
+    check_orbital_expression(tool, data)
+
+
+def test_orbital_based_from_molecule_orbital_expression_ch4_uhf_ccpvdz():
+    # load data computed with Fortran code
+    data = np.load(context.get_fn("test/data_orbitalbased_fortran_ch4_uhf_ccpvdz.npz"))
+    # test from_molecule initialization with exp_beta & check against Fortran code
+    molecule = Molecule.from_file(context.get_fn("test/ch4_uhf_ccpvdz.fchk"))
+    tool = OrbitalLocalTool(molecule, data["points"])
+    check_orbital_expression(tool, data)
+    # test from_molecule initialization without exp_beta & check against Fortran code
+    del molecule._exp_beta
+    molecule = Molecule.from_file(context.get_fn("test/ch4_uhf_ccpvdz.fchk"))
+    tool = OrbitalLocalTool(molecule, data["points"])
+    check_orbital_expression(tool, data)
+
+
+def test_orbital_based_from_file_orbital_expression_ch4_rhf_ccpvdz():
+    # load data computed with Fortran code
+    data = np.load(context.get_fn("test/data_orbitalbased_fortran_ch4_uhf_ccpvdz.npz"))
+    # test from_file initialization & check against Fortran code
+    tool = OrbitalLocalTool.from_file(context.get_fn("test/ch4_uhf_ccpvdz.fchk"), data["points"])
+    check_orbital_expression(tool, data)
+
+
+def test_orbital_based_from_molecule_orbital_expression_ch4_rhf_ccpvdz():
+    # load data computed with Fortran code
+    data = np.load(context.get_fn("test/data_orbitalbased_fortran_ch4_uhf_ccpvdz.npz"))
+    # test from_file initialization & check against Fortran code
+    molecule = Molecule.from_file(context.get_fn("test/ch4_rhf_ccpvdz.fchk"))
+    tool = OrbitalLocalTool(molecule, data["points"])
+    check_orbital_expression(tool, data)
+
+
+def test_orbital_based_h2o_b3lyp_sto3g():
+    # points array
+    points = np.array([[-3.,-3.,-3.], [-3.,-3., 0.], [-3.,-3., 3.], [-3., 0.,-3.], [-3., 0., 0.],
+                       [-3., 0., 3.], [-3., 3.,-3.], [-3., 3., 0.], [-3., 3., 3.], [ 0.,-3.,-3.],
+                       [ 0.,-3., 0.], [ 0.,-3., 3.], [ 0., 0.,-3.], [ 0., 0., 0.], [ 0., 0., 3.],
+                       [ 0., 3.,-3.], [ 0., 3., 0.], [ 0., 3., 3.], [ 3.,-3.,-3.], [ 3.,-3., 0.],
+                       [ 3.,-3., 3.], [ 3., 0.,-3.], [ 3., 0., 0.], [ 3., 0., 3.], [ 3., 3.,-3.],
+                       [ 3., 3., 0.], [ 3., 3., 3.]])
+    # initialize OrbitalLocalTool from_file
+    tool = OrbitalLocalTool.from_file(context.get_fn("test/water_b3lyp_sto3g.fchk"), points)
+    # check mep against Fortran code
+    expected = np.array([-0.01239766, -0.02982537, -0.02201149,   -0.01787292, -0.05682143,
+                         -0.02503563, -0.00405942, -0.00818772,   -0.00502268,  0.00321181,
+                         -0.03320573, -0.02788605,  0.02741914, 1290.21135500, -0.03319778,
+                          0.01428660,  0.10127092,  0.01518299,    0.01530548,  0.00197975,
+                         -0.00894206,  0.04330806,  0.03441681,   -0.00203017,  0.02272626,
+                          0.03730846,  0.01463959])
+    assert_array_almost_equal(tool.electrostatic_potential, expected, decimal=6)
+    # check spin chemical potential against manual calculation
+    result = tool.compute_spin_chemical_potential(25000.0)
+    assert_array_almost_equal(result, [0.10821228040, 0.10821228040], decimal=6)
+
+
+def test_orbital_based_from_file_elf_h2o_dimer():
+    # load data computed with NCIPLOT by E.R. Johnson and J. Contreras-Garcia
+    data = np.load(context.get_fn("test/data_elf_nciplot_h2o_dimer_pbe_sto3g.npz"))
+    # test from_file initialization & check ELF
+    filename = context.get_fn("test/h2o_dimer_pbe_sto3g.fchk")
+    tool = OrbitalLocalTool.from_file(filename, data["points"])
+    assert_array_almost_equal(tool.electron_localization_function, data["elf"], decimal=5)
+
+
+def test_orbital_based_from_molecule_elf_h2o_dimer():
+    # load data computed with NCIPLOT by E.R. Johnson and J. Contreras-Garcia
+    data = np.load(context.get_fn("test/data_elf_nciplot_h2o_dimer_pbe_sto3g.npz"))
+    # test from_molecule initialization & check ELF
+    molecule = Molecule.from_file(context.get_fn("test/h2o_dimer_pbe_sto3g.fchk"))
+    tool = OrbitalLocalTool(molecule, data["points"])
+    assert_array_almost_equal(tool.electron_localization_function, data["elf"], decimal=5)
