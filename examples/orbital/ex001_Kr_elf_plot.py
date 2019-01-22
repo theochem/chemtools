6a14f6324ac50ac92a3f268ea2b7382e0d41bfe7
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2019-01-22 19:13:31 +0100

    Remove HF/cc-pVDZ computation of Kr ELF example
    Add FCHk file for HF/cc-pVTZ calculation of Kr.

--- examples/orbital/ex001_Kr_elf_plot.py
+++ examples/orbital/ex001_Kr_elf_plot.py
@@ -1,92 +1,36 @@
 r"""
-======================================================
-EX1: Plot Krypton Electron Localization Function (ELF)
-======================================================
-
-1. Run a Hartree-Fock calculation with cc-pVDZ basis-set using HORTON.
-2. Build an OrbitalLocalTool.
-3. Plot Electron Localization Function (ELF) & radially weighted density.
+=================================================
+EX1: Krypton Electron Localization Function (ELF)
+=================================================
 """
+
 
 import numpy as np
 import matplotlib.pyplot as plt
-from horton import IOData, get_gobasis, CholeskyLinalgFactory, guess_core_hamiltonian
-from horton import compute_nucnuc, REffHam, AufbauOccModel, PlainSCFSolver, AtomicGrid
-from horton.meanfield import RTwoIndexTerm, RDirectTerm, RExchangeTerm
-from chemtools import Molecule, OrbitalLocalTool
+
+from chemtools import Molecule, ELF
+from horton import AtomicGrid
 
 
-# 1. Run a Hartree-Fock calculation with cc-pVDZ basis-set using HORTON
-
-# construct atom object placed at origin
-mol = IOData(title='Krypton')
-mol.coordinates = np.array([[0.0, 0.0, 0.0]])
-mol.numbers = np.array([36])
-
-# make a cc-pVDZ basis-set
-mol.obasis = get_gobasis(mol.coordinates, mol.numbers, 'cc-pVDZ')
-
-# create a linalg factory
-mol.lf = CholeskyLinalgFactory(mol.obasis.nbasis)
-
-# compute Gaussian integrals
-olp = mol.obasis.compute_overlap(mol.lf)
-kin = mol.obasis.compute_kinetic(mol.lf)
-na = mol.obasis.compute_nuclear_attraction(mol.coordinates, mol.pseudo_numbers, mol.lf)
-er = mol.obasis.compute_electron_repulsion(mol.lf)
-
-# create alpha orbitals
-mol.exp_alpha = mol.lf.create_expansion()
-
-# initial guess
-guess_core_hamiltonian(olp, kin, na, mol.exp_alpha)
-
-# construct restricted HF effective Hamiltonian
-external = {'nn': compute_nucnuc(mol.coordinates, mol.pseudo_numbers)}
-terms = [RTwoIndexTerm(kin, 'kin'),
-         RDirectTerm(er, 'hartree'),
-         RExchangeTerm(er, 'x_hf'),
-         RTwoIndexTerm(na, 'ne'),
-        ]
-ham = REffHam(terms, external)
-
-# select orbital occupation scheme (1 alpha electron)
-occ_model = AufbauOccModel(18)
-
-# compute WFN with plain SCF
-scf_solver = PlainSCFSolver(1e-6)
-scf_solver(ham, mol.lf, olp, occ_model, mol.exp_alpha)
-
-# 2. Post-processing
-# ------------------
-
-# construct an atomic grid
-grid = AtomicGrid(mol.numbers[0], mol.pseudo_numbers[0], mol.coordinates[0],
+# load atom and construct and atomic grid
+filename = 'data/examples/atom_kr_hf_ccpvdz.fchk'
+molecule = Molecule.from_file(filename)
+grid = AtomicGrid(molecule.numbers[0], molecule.pseudo_numbers[0], molecule.coordinates[0],
                   agspec='exp:0.005:10.0:200:194', random_rotate=False)
 
-# get radial distribution
-radii = grid.rgrid.radii
+# construct an instance of ELF
+tool = ELF.from_molecule(molecule, spin='ab', index=None, grid=grid)
 
-# construct an OrbitalLocalTool
-molecule = Molecule(mol)
-tool = OrbitalLocalTool(molecule, grid.points)
-
-# calculate spherically-averaged density & Electron Localization Function (ELF)
+# calculate spherically-averaged density ($4.0 \pi r^2 \rho$) & ELF
+elf = grid.get_spherical_average(tool.values)
 dens = grid.get_spherical_average(tool.density)
-elf = grid.get_spherical_average(tool.electron_localization_function)
+dens *= (4.0 * np.pi * grid.rgrid.radii**2)
 
-# calculate radially weighted density, i.e. $4.0 \pi r^2 \rho$
-rdens = 4.0 * np.pi * radii * radii * dens
-
-# 3. Plot Electron Localization Function (ELF) & radially weighted density.
-
-# compute log10 of radii
-logx = np.array([np.math.log(radius, 10.) for radius in grid.rgrid.radii])
-
-# plot ElF & radially weighted density
+# compute log10 of radii & plot ElF and (scaled) radially weighted density
+logx = np.log10(grid.rgrid.radii)
 plt.plot(logx, elf, linewidth=2, label='Electron Localization Function')
-plt.plot(logx, 0.01875 * rdens, linewidth=2, label='Scaled radially weighted density')
-plt.legend(loc='upper right', fontsize=10)
+plt.plot(logx, 0.01875 * dens, linestyle='--', linewidth=2, label='Radially Weighted Density (Scaled)')
+plt.legend(loc='upper right', fontsize=8, frameon=False)
 plt.xlim(-2., 1.)
-plt.xlabel('log$_{10}$(radial distance from the nucleus / $a_0$ )')
+plt.xlabel('log$_{10}$(radial distance from the nucleus / $a_0$)')
 plt.show()
