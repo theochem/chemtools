eddf39dafc37665e20a5c226fc7786521ac82051
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2019-04-24 20:52:38 +0200

    Fix filepath & description in NCI example

--- examples/density/ex001_nci_plot.py
+++ examples/density/ex001_nci_plot.py
@@ -1,7 +1,10 @@
 r"""
-===================================================
-EX1: Non-Covalent Interactions (NCI) of water dimer
-===================================================
+====================================
+EX1: Non-Covalent Interactions (NCI)
+====================================
+
+Compute non-covalent interaction (NCI) and visualize it.
+
 """
 
 from chemtools import NCI
@@ -9,7 +12,7 @@ from chemtools import NCI
 
 # 1. Build NCI model using default settings
 
-nci = NCI.from_file('data/examples/h2o_dimer_pbe_sto3g.fchk')
+nci = NCI.from_file('h2o_dimer.fchk')
 
 # 2. Dump files/scripts for visualizing NCI
 #    Files generated are h2o_dimer-dens.cube, h2o_dimer-grad.cube, & h2o_dimer.vmd
