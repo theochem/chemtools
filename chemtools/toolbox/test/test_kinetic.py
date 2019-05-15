c1dc9d3080f015aa9fdd1674a1a82d6e9345d782
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2019-05-15 19:19:56 +0200

    Change " to ' in test_kinetic.py

--- chemtools/toolbox/test/test_kinetic.py
+++ chemtools/toolbox/test/test_kinetic.py
@@ -39,12 +39,12 @@ except ImportError:
 def check_kinetic_energy_density(tool, data):
     """Test KineticEnergyDensity against stored data arrays."""
     # check density & gradient
-    assert_array_almost_equal(tool.density, data["density"], decimal=6)
+    assert_array_almost_equal(tool.density, data['density'], decimal=6)
     # check kinetic energy density
-    assert_array_almost_equal(tool.ked_positive_definite, data["ke_positive_definite"], decimal=6)
-    assert_array_almost_equal(tool.ked_weizsacker, data["ke_weizsacker"], decimal=6)
+    assert_array_almost_equal(tool.ked_positive_definite, data['ke_positive_definite'], decimal=6)
+    assert_array_almost_equal(tool.ked_weizsacker, data['ke_weizsacker'], decimal=6)
     # TODO: Why assert_array_almost_equal doesn't work for ke_thomas_fermi
-    assert_allclose(tool.ked_thomas_fermi, data["ke_thomas_fermi"], rtol=1e-08, atol=1e-08)
+    assert_allclose(tool.ked_thomas_fermi, data['ke_thomas_fermi'], rtol=1e-08, atol=1e-08)
 
 
 def test_kinetic_from_file_ch4_uhf_ccpvdz():
@@ -53,7 +53,7 @@ def test_kinetic_from_file_ch4_uhf_ccpvdz():
         data = np.load(str(fname))
     # test from_file initialization & check against Fortran code
     with path("chemtools.data", "ch4_uhf_ccpvdz.fchk") as fname:
-        tool = KED.from_file(fname, data["points"])
+        tool = KED.from_file(fname, data['points'])
     check_kinetic_energy_density(tool, data)
 
 
@@ -64,14 +64,14 @@ def test_kinetic_from_molecule_ch4_uhf_ccpvdz():
     # test from_molecule initialization with exp_beta & check against Fortran code
     with path("chemtools.data", "ch4_uhf_ccpvdz.fchk") as fname:
         molecule = Molecule.from_file(fname)
-    tool = KED.from_molecule(molecule, data["points"])
+    tool = KED.from_molecule(molecule, data['points'])
     print('tool = ', tool)
     check_kinetic_energy_density(tool, data)
     # test from_molecule initialization without exp_beta & check against Fortran code
     del molecule._exp_beta
     with path("chemtools.data", "ch4_uhf_ccpvdz.fchk") as fname:
         molecule = Molecule.from_file(fname)
-    tool = KED.from_molecule(molecule, data["points"])
+    tool = KED.from_molecule(molecule, data['points'])
     check_kinetic_energy_density(tool, data)
 
 
@@ -81,7 +81,7 @@ def test_kinetic_from_file_ch4_rhf_ccpvdz():
         data = np.load(str(fname))
     # test from_file initialization & check against Fortran code
     with path("chemtools.data", "ch4_uhf_ccpvdz.fchk") as fname:
-        tool = KED.from_file(fname, data["points"])
+        tool = KED.from_file(fname, data['points'])
     check_kinetic_energy_density(tool, data)
 
 
@@ -92,5 +92,5 @@ def test_kinetic_from_molecule_ch4_rhf_ccpvdz():
     # test from_file initialization & check against Fortran code
     with path("chemtools.data", "ch4_rhf_ccpvdz.fchk") as fname:
         molecule = Molecule.from_file(fname)
-    tool = KED.from_molecule(molecule, data["points"])
+    tool = KED.from_molecule(molecule, data['points'])
     check_kinetic_energy_density(tool, data)
