245d2156085d00de10e42c1ef30ec34e8ca7b1c4
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2019-05-19 10:57:44 +0200

    Fix Molecule class to load non-wavefunction files

--- chemtools/wrappers/molecule.py
+++ chemtools/wrappers/molecule.py
@@ -62,13 +62,19 @@ class Molecule(object):
         self._coordinates = self._iodata.coordinates
         self._numbers = self._iodata.numbers
 
-        # assign alpha orbital expression
-        self._exp_alpha = self._iodata.exp_alpha
-        # assign beta orbital expression
-        if hasattr(self._iodata, "exp_beta") and self._iodata.exp_beta is not None:
-            self._exp_beta = self._iodata.exp_beta
+        print(self._iodata)
+        print(dir(self._iodata))
+        if hasattr(self._iodata, 'exp_alpha'):
+            # assign alpha orbital expression
+            self._exp_alpha = self._iodata.exp_alpha
+            # assign beta orbital expression
+            if hasattr(self._iodata, 'exp_beta') and self._iodata.exp_beta is not None:
+                self._exp_beta = self._iodata.exp_beta
+            else:
+                self._exp_beta = self._iodata.exp_alpha
         else:
-            self._exp_beta = self._iodata.exp_alpha
+            self._exp_alpha = None
+            self._exp_beta = None
 
     @classmethod
     def from_file(cls, fname):
