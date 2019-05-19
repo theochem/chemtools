80536693b1b85e25ddee4fb3f8e32253ab56cb92
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2019-05-19 05:19:31 +0200

    Refactor BeckeGrid class

--- chemtools/wrappers/beckegrid.py
+++ chemtools/wrappers/beckegrid.py
@@ -31,43 +31,10 @@ __all__ = ['BeckeGrid']
 
 
 class BeckeGrid(object):
-    """Grid class for wrapping grid module from HORTON package.
-
-    Usage
-    -----
-    Initialization:
-    >>> grid_model = BeckeGrid.from_molecule(mol, 'fine')
-    >>> my_grid = grid_model.grid
-    Or
-    >>> grid_model = BeckeGrid(mol.coordinates, mol.numbers, mol.pseudo_numbers, 'fine')
-    >>> my_grid = grid_model.grid
-
-    Change grid:
-    >>> grid_model.grid_type = 'coarse'
-    >>> new_grid_1 = grid_model.grid
-    Or
-    >>> grid_model.grid_type = 'linear:1e-5:2e1:120:110'
-    >>> new_grid_2 = grid_model.grid
-
-    Attributes
-    ----------
-    grid_type : str
-        Information about current grid properties.
-    rname : str
-        Type of customized radial grid.
-    rpoint : int
-        The number of points for the angular Lebedev-Laikov grid.
-    rrad : int
-        The number of radial grid points.
-    rrange : tuple(float, float)
-        The first and the last radial grid point in angstroms.
-    grid : BeckeMolGrid, read-only
-        Generated Becke integration grid for given parameters.
-
-    """
+    """Grid class for wrapping grid module from HORTON package."""
 
     def __init__(self, coordinates, numbers, pseudo_numbers, specification='medium', k=3,
-                 random_rotate=False, mode='discard'):
+                 random_rotate=False):
         """Initialize class.
 
         Parameters
@@ -84,202 +51,101 @@ class BeckeGrid(object):
             Here 'rname' denotes the type of radial grid and can be chosen from ['linear', 'exp',
             'power'], 'rmin' and 'rmax' specify the first and last radial grid points in angstrom,
             'nrad' specify the number of radial grid points, and 'nang' specify the number of
-            angular Lebedev-Laikov grid.
+            angular Lebedev-Laikov grid. The 'nang' can be chosen from (6, 14, 26, 38, 50, 74, 86,
+            110, 146, 170, 194, 230, 266, 302, 350, 434, 590, 770, 974, 1202, 1454, 1730, 2030,
+            2354, 2702, 3074, 3470, 3890, 4334, 4802, 5294, 5810).
         k : int, optional
             The order of the switching function in Becke's weighting scheme.
         random_rotate : bool, optional
             Flag to control random rotation of spherical grids.
-        mode : str, optional
-            Select one of the following options regarding atomic subgrids.
-            Avail choices: ['discard', 'keep', 'only'].
 
         """
         self._coordinates = coordinates
         self._numbers = numbers
-        self._pseudo_n = pseudo_numbers
-        self._grid_type = None  # used to store grid info for default type
+        self._pseudo_numbers = pseudo_numbers
         self._k = k
         self._random_rotate = random_rotate
-        if mode in ['discard', 'keep', 'only']:
-            self._mode = mode
-        else:
-            raise ValueError('Argument mode={0} is not valid!'.format(mode))
-        # grid type specification
-        self._custom_type = [None] * 5
-        self.grid_type = specification
+        self.specification = specification
 
-    @property
-    def grid_type(self):
-        """Type of current grid."""
-        if self._grid_type:
-            return self._grid_type
-        else:
-            return ':'.join(map(str, self._custom_type))
-
-    @grid_type.setter
-    def grid_type(self, value):
-        """Set the Property of Grid.
-
-        Parameters
-        ----------
-        value : str
-            One of preset types from ('coarse', 'medium', 'fine', 'veryfine',
-            'ultrafine', 'insane') or a string of five properties split by ':'
-        """
-        valid_types = ('coarse', 'medium', 'fine', 'veryfine', 'ultrafine', 'insane')
-        if value in valid_types:
-            # set custom to None
-            self._custom_type = [None] * 5
-            # set new grid type
-            self._grid_type = value
-        else:
-            # split input str
-            ind_set = value.split(':')
-            if len(ind_set) != 5:
-                raise ValueError('Input grid_type: {} is not valid'.format(value))
-            ind_set[1:3] = map(float, ind_set[1:3])
-            ind_set[3:] = map(int, ind_set[3:])
-            self.rname = ind_set[0]
-            self.rrange = ind_set[1:3]
-            self.rrad = ind_set[3]
-            self.rpoint = ind_set[4]
-            # map str to float and int
-
-    @property
-    def rname(self):
-        """Type of the radial grid."""
-        return self._custom_type[0]
-
-    @rname.setter
-    def rname(self, value):
-        """Set the type of the radial grid from the preset types.
-
-        Parameters
-        ----------
-        value : str
-            The value of radial grid name,
-            valid choices are ['linear', 'exp', 'power'].
-        """
-        valid_inputs = ('linear', 'exp', 'power')
-        if value not in valid_inputs:
-            raise ValueError('Input value: {} is not valid'.format(value))
-        self._custom_type[0] = value
-        self._reset_grid_type()
-
-    @property
-    def rrange(self):
-        """Specify the first and the last radial grid point in angstroms."""
-        return self._custom_type[1:3]
-
-    @rrange.setter
-    def rrange(self, value):
-        """Specify the first and the last radial grid point in angstroms."""
-        start, stop = value[:]
-        if isinstance(start, (int, float)) and isinstance(stop, (int, float)):
-            self._custom_type[1:3] = start, stop
-            self._reset_grid_type()
-        else:
-            raise TypeError("Given values are not int")
-
-    @property
-    def rrad(self):
-        """Return the number of radial grid points."""
-        return self._custom_type[3]
-
-    @rrad.setter
-    def rrad(self, value):
-        """Return the number of radial grid points."""
-        if not isinstance(value, int):
-            raise TypeError('Given value is not an int')
-        self._custom_type[3] = value
-        self._reset_grid_type()
-
-    @property
-    def rpoint(self):
-        """Return the number of points for the angular Lebedev-Laikov grid."""
-        return self._custom_type[4]
-
-    @rpoint.setter
-    def rpoint(self, value):
-        """Return the number of points for the angular Lebedev-Laikov grid.
-
-        Parameters
-        ----------
-        value : int
-            The value need to be in the following list:
-            (6, 14, 26, 38, 50, 74, 86, 110, 146, 170, 194, 230, 266,
-             302, 350, 434, 590, 770, 974, 1202, 1454, 1730, 2030,
-             2354, 2702, 3074, 3470, 3890, 4334, 4802, 5294, 5810)
-        """
-        if not isinstance(value, int):
-            raise TypeError('Given value is not an int')
-        valid_ill = (6, 14, 26, 38, 50, 74, 86, 110, 146, 170, 194, 230, 266,
-                     302, 350, 434, 590, 770, 974, 1202, 1454, 1730, 2030,
-                     2354, 2702, 3074, 3470, 3890, 4334, 4802, 5294, 5810)
-        if value not in valid_ill:
-            raise ValueError("""Given value is not a valid value,
-                check 'http://theochem.github.io/horton/2.1.0/te\
-                ch_ref_grids.html#ref-grids' for reference""")
-        self._custom_type[4] = value
-        self._reset_grid_type()
-
-    @property
-    def grid(self):
-        """Return a grid object based on set property."""
-        if None in self._custom_type and self._grid_type is None:
-            raise ValueError("Don't have enough info to generate a grid")
-        return BeckeMolGrid(
-            self._coordinates,
-            self._numbers,
-            self._pseudo_n,
-            agspec=self.grid_type,
-            k=self._k,
-            random_rotate=self._random_rotate,
-            mode=self._mode)
+        self._grid = BeckeMolGrid(self.coordinates, self.numbers, self.pseudo_numbers,
+                                  agspec=self.specification, k=k,
+                                  random_rotate=random_rotate, mode='discard')
 
     @classmethod
-    def from_molecule(cls,
-                      mol,
-                      grid_type='medium',
-                      k=3,
-                      random_rotate=False,
-                      mode='discard'):
-        """Construct a grid for given molecule.
+    def from_molecule(cls, mol, specification='medium', k=3, random_rotate=False):
+        """Initialize the class given an instance of Molecule.
 
         Parameters
         ----------
         mol : instance of Molecule
             Instance of Molecule class.
-        grid_type : str, optional
-            Information about current grid properties.
+        specification : str, optional
+            Specification of grid. Either choose from ['coarse', 'medium', 'fine', 'veryfine',
+            'ultrafine', 'insane'] or provide a string of 'rname:rmin:rmax:nrad:nang' format.
+            Here 'rname' denotes the type of radial grid and can be chosen from ['linear', 'exp',
+            'power'], 'rmin' and 'rmax' specify the first and last radial grid points in angstrom,
+            'nrad' specify the number of radial grid points, and 'nang' specify the number of
+            angular Lebedev-Laikov grid. The 'nang' can be chosen from (6, 14, 26, 38, 50, 74, 86,
+            110, 146, 170, 194, 230, 266, 302, 350, 434, 590, 770, 974, 1202, 1454, 1730, 2030,
+            2354, 2702, 3074, 3470, 3890, 4334, 4802, 5294, 5810).
         k : int, optional
             The order of the switching function in Becke's weighting scheme.
         random_rotate : bool, optional
             Flag to control random rotation of spherical grids.
-        mode : str, optional
-            Select one of the following options regarding atomic subgrids.
 
-        Returns
-        -------
-        BeckeGrid
-            The Grid object for constructing integral grid
-
-        Raises
-        ------
-        ValueError
-            The given molecule is not a valid instance
         """
         if not isinstance(mol, Molecule):
-            raise TypeError('Given molecule object is not valid')
-        return cls(
-            mol.coordinates,
-            mol.numbers,
-            mol.pseudo_numbers,
-            specification=grid_type,
-            k=k,
-            random_rotate=random_rotate,
-            mode=mode)
+            raise TypeError('Argument mole should be an instance of Molecule class.')
+        return cls(mol.coordinates, mol.numbers, mol.pseudo_numbers, specification=specification,
+                   k=k, random_rotate=random_rotate)
 
-    def _reset_grid_type(self):
-        """Set self._grid_type back to None."""
-        self._grid_type = None
+    @classmethod
+    def from_file(cls, fname, specification='medium', k=3, random_rotate=False):
+        """Initialize the class given an instance of Molecule.
+
+        Parameters
+        ----------
+        fname : str
+            Path to molecule's files.
+        specification : str, optional
+            Specification of grid. Either choose from ['coarse', 'medium', 'fine', 'veryfine',
+            'ultrafine', 'insane'] or provide a string of 'rname:rmin:rmax:nrad:nang' format.
+            Here 'rname' denotes the type of radial grid and can be chosen from ['linear', 'exp',
+            'power'], 'rmin' and 'rmax' specify the first and last radial grid points in angstrom,
+            'nrad' specify the number of radial grid points, and 'nang' specify the number of
+            angular Lebedev-Laikov grid. The 'nang' can be chosen from (6, 14, 26, 38, 50, 74, 86,
+            110, 146, 170, 194, 230, 266, 302, 350, 434, 590, 770, 974, 1202, 1454, 1730, 2030,
+            2354, 2702, 3074, 3470, 3890, 4334, 4802, 5294, 5810).
+        k : int, optional
+            The order of the switching function in Becke's weighting scheme.
+        random_rotate : bool, optional
+            Flag to control random rotation of spherical grids.
+
+        """
+        mol = Molecule.from_file(fname)
+        return cls.from_molecule(mol, specification, k, random_rotate)
+
+    @property
+    def coordinates(self):
+        """Cartesian coordinates of atomic centers."""
+        return self._coordinates
+
+    @property
+    def numbers(self):
+        """Atomic number of atomic centers."""
+        return self._numbers
+
+    @property
+    def pseudo_numbers(self):
+        """Pseudo atomic number of atomic centers."""
+        return self._pseudo_numbers
+
+    @property
+    def points(self):
+        """Cartesian coordinates of grid points."""
+        return self._grid.points
+
+    @property
+    def npoints(self):
+        """Number of grid points."""
+        return self._grid.points
--- chemtools/wrappers/test/test_grid.py
+++ chemtools/wrappers/test/test_grid.py
@@ -22,163 +22,31 @@
 # --
 """Test chemtools.wrappers.grid."""
 
-from unittest import TestCase
 
-import numpy as np
-from horton import BeckeMolGrid
-from importlib_resources import path
+try:
+    from importlib_resources import path
+except ImportError:
+    from importlib.resources import path
+
+from numpy.testing import assert_raises, assert_allclose
 
 from chemtools.wrappers.beckegrid import BeckeGrid
 from chemtools.wrappers.molecule import Molecule
 
 
+def test_wrapper_grid_raises():
+    with path('chemtools.data', 'ch4_uhf_ccpvdz.fchk') as fpath:
+        assert_raises(TypeError, BeckeGrid.from_molecule, fpath, 'exp:1e-5:20:40:50')
+        assert_raises(ValueError, BeckeGrid.from_file, fpath, 'ex:1e-5:20:40:50')
+        assert_raises(ValueError, BeckeGrid.from_file, fpath, 'exp:1e-5:-20:40:50')
+        assert_raises(ValueError, BeckeGrid.from_file, fpath, 'exp:1e-5:20:40:10')
 
 
-class TestGrid(TestCase):
-    """Grid test class."""
-
-    def setUp(self):
-        """Set up function for all tests."""
-        with path('chemtools.data', 'water_b3lyp_sto3g.fchk') as file_path:
-            self.mol = Molecule.from_file(file_path)
-        self.grid_mol = BeckeGrid(self.mol.coordinates, self.mol.numbers, self.mol.pseudo_numbers)
-
-    def test_string_init(self):
-        """Test Grid object initialization."""
-        grid = BeckeGrid(self.mol.coordinates, self.mol.numbers, self.mol.pseudo_numbers,
-                         specification='exp:1e-5:20:40:50')
-        # assert grid._grid_type is None
-        assert grid.rrad == 40
-        assert grid.rpoint == 50
-
-    def test_init(self):
-        """Check initial values and attributs."""
-        assert isinstance(self.grid_mol, BeckeGrid)
-        assert np.allclose(self.grid_mol._coordinates, self.mol.coordinates)
-        assert np.allclose(self.grid_mol._numbers, self.mol.numbers)
-        assert np.allclose(self.grid_mol._pseudo_n, self.mol.pseudo_numbers)
-        assert self.grid_mol._grid_type == 'medium'
-        assert self.grid_mol._custom_type == [None, None, None, None, None]
-        assert self.grid_mol.grid_type == 'medium'
-        assert self.grid_mol._k == 3
-        assert self.grid_mol._random_rotate is False
-        assert self.grid_mol._mode == "discard"
-
-    def test_error_type(self):
-        """Check invalid string format."""
-        with self.assertRaises(ValueError):
-            self.grid_mol.grid_type = 'linear:1e-5:2e1:120'
-        with self.assertRaises(ValueError):
-            self.grid_mol.grid_type = 'coarses'
-        with self.assertRaises(ValueError):
-            self.grid_mol.grid_type = 'lin:1e-5:2e1:120:110'
-        # fail to converge str(xxx) to float
-        with self.assertRaises(ValueError):
-            self.grid_mol.grid_type = 'linear:xxxx:2e1:120:110'
-        with self.assertRaises(TypeError):
-            self.grid_mol.rrad = 120.
-        with self.assertRaises(TypeError):
-            self.grid_mol.rpoint = 120.
-        with self.assertRaises(ValueError):
-            self.grid_mol.rpoint = 119
-        with self.assertRaises(TypeError):
-            self.grid_mol.rrange = ('start', 'end')
-
-    def test_switching_format(self):
-        """Change grid format between two styles."""
-        # set grid with string
-        ref_grid = 'linear:1e-5:2e1:120:110'
-        self.grid_mol.grid_type = ref_grid
-        # check return grid information
-        contents = self.grid_mol.grid_type.split(':')
-        assert str(contents[0]) == 'linear'
-        assert float(contents[1]) == 1e-5
-        assert float(contents[2]) == 20
-        assert int(contents[3]) == 120
-        assert int(contents[4]) == 110
-        # assert self.grid_mol._grid_type is None
-        assert self.grid_mol.rname == 'linear'
-        assert np.allclose(self.grid_mol.rrange, (1e-5, 20))
-        assert self.grid_mol.rrad == 120
-        assert self.grid_mol.rpoint == 110
-
-        # switch back to test grid
-        self.grid_mol.grid_type = 'fine'
-        assert self.grid_mol.grid_type == 'fine'
-        self.grid_mol.rname = 'exp'
-        self.grid_mol.rrange = [0.0005, 20]
-        # test not have enough info for generate a grid
-        with self.assertRaises(ValueError):
-            grid = self.grid_mol.grid
-        self.grid_mol.rrad = 40
-        self.grid_mol.rpoint = 50
-
-        # compare two grids
-        grid = self.grid_mol.grid
-        ref_grid = BeckeGrid.from_molecule(self.mol, 'exp:5e-4:2e1:40:50').grid
-        assert np.array_equal(grid.points, ref_grid.points)
-        assert np.array_equal(grid.weights, ref_grid.weights)
-        # assert self.grid_mol._grid_type is None
-
-    def test_form_grid(self):
-        """Generate the same grid."""
-        gene_grid = self.grid_mol.grid
-        ref_grid = BeckeMolGrid(
-            self.mol.coordinates,
-            self.mol.numbers,
-            self.mol.pseudo_number,
-            random_rotate=False)
-        assert np.array_equal(gene_grid.points, ref_grid.points)
-        assert np.array_equal(gene_grid.weights, ref_grid.weights)
-
-        # generate a grid from str and compare
-        self.grid_mol.grid_type = 'linear:1e-5:2e1:50:50'
-        gene_grid = self.grid_mol.grid
-        ref_grid = BeckeMolGrid(
-            self.mol.coordinates,
-            self.mol.numbers,
-            self.mol.pseudo_number,
-            agspec='linear:1e-5:2e1:50:50',
-            random_rotate=False)
-        assert np.array_equal(gene_grid.points, ref_grid.points)
-        assert np.array_equal(gene_grid.weights, ref_grid.weights)
-
-        # change back to preset grid
-        self.grid_mol.grid_type = 'coarse'
-        gene_grid = self.grid_mol.grid
-        ref_grid = BeckeMolGrid(
-            self.mol.coordinates,
-            self.mol.numbers,
-            self.mol.pseudo_number,
-            agspec='coarse',
-            random_rotate=False)
-        assert np.array_equal(gene_grid.points, ref_grid.points)
-        assert np.array_equal(gene_grid.weights, ref_grid.weights)
-
-        # specify by each elements
-        self.grid_mol.rname = 'exp'
-        self.grid_mol.rrange = [1e-4, 18]
-        self.grid_mol.rrad = 30
-        self.grid_mol.rpoint = 38
-        gene_grid = self.grid_mol.grid
-        ref_grid = BeckeMolGrid(
-            self.mol.coordinates,
-            self.mol.numbers,
-            self.mol.pseudo_number,
-            agspec='exp:1e-4:18:30:38',
-            random_rotate=False)
-        assert np.array_equal(gene_grid.points, ref_grid.points)
-        assert np.array_equal(gene_grid.weights, ref_grid.weights)
-
-    def test_grid_from_molecule(self):
-        """Test from_molecule generate the same grid."""
-        ref_grid = BeckeGrid.from_molecule(self.mol).grid
-        gene_grid = self.grid_mol.grid
-        assert np.array_equal(gene_grid.points, ref_grid.points)
-        assert np.array_equal(gene_grid.weights, ref_grid.weights)
-
-        # set to wrong mode
-        with self.assertRaises(ValueError):
-            BeckeGrid.from_molecule(self.mol, mode='random')
-        with self.assertRaises(TypeError):
-            BeckeGrid.from_molecule('random')
+def test_wrapper_grid_ch4():
+    with path('chemtools.data', 'ch4_uhf_ccpvdz.fchk') as fpath:
+        mol = Molecule.from_file(fpath)
+    grid = BeckeGrid(mol.coordinates, mol.numbers, mol.pseudo_numbers, 'exp:1e-5:20:40:50')
+    # check grid basics
+    assert_allclose(mol.coordinates, grid.coordinates, rtol=0., atol=1.e-7)
+    assert_allclose(mol.numbers, grid.numbers, rtol=0., atol=1.e-7)
+    assert_allclose(mol.pseudo_numbers, grid.pseudo_numbers, rtol=0., atol=1.e-7)
