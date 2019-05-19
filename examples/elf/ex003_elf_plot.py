00e367e089640b705486bbb83c205b29fb2627d4
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2019-05-19 03:34:20 +0200

    Update ELF example using UniformGrid.from_molecule

--- examples/elf/ex003_elf_plot.py
+++ examples/elf/ex003_elf_plot.py
@@ -12,7 +12,7 @@ from chemtools import ELF, UniformGrid, Molecule
 # 1. Build Molecule, UnifromGrid and ELF model
 
 mol = Molecule.from_file('chonh2.fchk')
-cub = UniformGrid.from_file('chonh2.fchk', spacing=0.1, threshold=5.0)
+cub = UniformGrid.from_molecule(mol, spacing=0.1, threshold=5.0)
 elf = ELF.from_molecule(mol, grid=cub, trans='hyperbolic', trans_k=1, trans_a=1)
 
 # 2. Generate cube file(s) and script for visualizing ELF
@@ -20,6 +20,7 @@ elf = ELF.from_molecule(mol, grid=cub, trans='hyperbolic', trans_k=1, trans_a=1)
 #    To visualize the iso-surface, use command: $ vmd -e chonh2.vmd
 
 elf.generate_scripts('chonh2', isosurf=0.8)
+
 
 # DISCARD BELOW:
 # the code below is for displaying the ELF image on the website, you should remove it
