35d04b38b66adfb6b1bfe6c75df015dd620a566d
Taewon D. Kim <kimt33@mcmaster.ca>
2019-05-20 02:39:21 +0200

    Update example to use mesh.plane_grid
    Construct a mesh that uses only the points on the plane, which means that this
    example runs a lot faster than before.
    
    Issues:
    The API for plane_grid isn't that good, so the mesh is much larger than it needs
    to be.

--- examples/conceptual/ex010_local_quadratic_ff_fmo_contour_plot.py
+++ examples/conceptual/ex010_local_quadratic_ff_fmo_contour_plot.py
@@ -12,49 +12,52 @@ EX10: 2D-Contours Quadratic Fukui Function (FMO Approach)
 
 import numpy as np
 import matplotlib.pyplot as plt
-from chemtools import LocalConceptualDFT
+from chemtools import LocalConceptualDFT, plane_mesh
+
 
 # 1. Make a 2D grid in xy-plane (molecular plane).
 
-# sample points in x and y direction (1D arrays); half-plane containing molecule is sampled
-x = np.arange(-3., 4., 0.1)
-y = np.arange(0., 3., 0.1)
-z = np.zeros_like(x)
-
-# make a mesh from 1D arrays
-xv, yv, zv = np.meshgrid(x, y, z)
-
-# turn mesh into 3D array
-xyz = np.vstack((xv, yv, zv)).reshape((3, -1)).T
-xyz = np.ascontiguousarray(xyz, dtype=np.float64)
+xyz = plane_mesh(
+    np.array(
+        [
+            [2.27823914e00, 4.13899085e-07, 3.12033662e-07],
+            [1.01154892e-02, 1.09802629e-07, -6.99333116e-07],
+            [-1.09577141e00, 1.77311416e00, 1.42544321e-07],
+        ]
+    ),
+    0.1,
+    6,
+)
 
 # 2. Build a quadratic energy models using FMO approach
 
 # path to molecule's fchk file
-file_path = 'ch2o_q+0.fchk'
+file_path = "ch2o_q+0.fchk"
 # build a quadratic global conceptual DFT tool
-tool = LocalConceptualDFT.from_file(file_path, model='quadratic', points=xyz)
+tool = LocalConceptualDFT.from_file(
+    file_path, model="quadratic", points=xyz.reshape(-1, 3)
+)
 
 # 3. Evaluate quadratic Fukui function on the grid points.
 
-ff_quad = tool.fukui_function.reshape(xv.shape)[:, :, 0]
+ff_quad = tool.fukui_function.reshape(xyz.shape[:2])
 
 # 4. Plot 2D-contour plots of quadratic Fukui function.
 
 # sample contour lines
 levels = np.array([0.001 * n * n for n in range(500)])
 # plot contours of quadratic Fukui function
-plt.contour(x, y, ff_quad, levels)
+plt.contour(xyz[:, :, 0], xyz[:, :, 1], ff_quad, levels)
 
 # plot atomic centers
 x_atoms, y_atoms = tool.coordinates[:, 0], tool.coordinates[:, 1]
-plt.plot(x_atoms, y_atoms, marker='o', linestyle='', markersize=15, color='k')
+plt.plot(x_atoms, y_atoms, marker="o", linestyle="", markersize=15, color="k")
 
 # setting axis range & title
-plt.axis([-3.0, 4.0, 0.0, 3.0])
-plt.title('Quadratic Fukui Function Contour Plots', fontweight='bold')
+plt.axis([-3.0, 4.3, -4.0, 4.0])
+plt.title("Quadratic Fukui Function Contour Plots", fontweight="bold")
 # add axis label
-plt.xlabel('x-axis', fontsize=12)
-plt.ylabel('y-axis', fontsize=12)
+plt.xlabel("x-axis", fontsize=12)
+plt.ylabel("y-axis", fontsize=12)
 # show plot
 plt.show()
