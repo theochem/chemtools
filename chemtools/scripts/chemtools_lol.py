53c81e06b15c7911fbb2a9a0eb92a5474650fd96
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2019-05-21 03:31:23 +0200

    Rename input/output in LOL script

--- chemtools/scripts/chemtools_lol.py
+++ chemtools/scripts/chemtools_lol.py
@@ -36,15 +36,15 @@ Generate a VMD (Visual Molecular Dynamics) script as well as cube file required
 for visualizing Localized Orbital Locator (LOL) with VMD package.
 
 The generated files include:
-  fname_output.vmd             The VMD script.
-  fname_output-lol.cube        The LOL cube file.
+  output.vmd             The VMD script.
+  output-lol.cube        The LOL cube file.
 
 If VMD is setup on your system, you can visualize LOL with the command below:
-    $ vmd -e fname_output.vmd
+    $ vmd -e output.vmd
 For instruction on how to open the script from the VMD interactive environment,
 please refer to ChemTools website.
 
-Note: The fname_output.vmd script requires fname_output-lol.cube to plot LOL
+Note: The output.vmd script requires output-lol.cube to plot LOL
       in VMD software (they files should be all in the same directory).
 """
 
@@ -54,11 +54,11 @@ def parse_args_lol(subparser):
 
     # required arguments
     subparser.add_argument(
-        'file_wfn',
+        'fname',
         help='wave-function file. Supported formats: fchk, mkl, molden.input, wfn.')
 
     subparser.add_argument(
-        'output_name', help='name of generated cube file and vmd script.')
+        'output', help='name of generated cube file and vmd script.')
 
     # optional arguments
     subparser.add_argument(
@@ -110,7 +110,7 @@ def parse_args_lol(subparser):
 def main_lol(args):
     """Build LOL model and dump VMD script and cube files for visualizing LOL."""
     # load molecule
-    mol = Molecule.from_file(args.file_wfn)
+    mol = Molecule.from_file(args.fname)
 
     # make cubic grid
     if args.cube.endswith('.cube'):
@@ -128,4 +128,4 @@ def main_lol(args):
                             trans_a=args.trans_a, denscut=args.denscut)
 
     # dump file/script for visualizing LOL
-    lol.generate_scripts(args.output_name, isosurf=args.isosurface)
+    lol.generate_scripts(args.output, isosurf=args.isosurface)
