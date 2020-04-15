bfbfec3a1877a4aac211b5e8ee25a59371c2d5f5
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2020-04-15 06:13:57 -0700

    Trim down MOTBasedTool class

--- chemtools/toolbox/motbased.py
+++ chemtools/toolbox/motbased.py
@@ -27,9 +27,7 @@
 import numpy as np
 
 from chemtools.utils.utils import doc_inherit
-from chemtools.utils.cube import UniformGrid
 from chemtools.orbstools.partition import OrbitalPartitionTools
-from chemtools.outputs.vmd import print_vmd_script_isosurface
 from chemtools.wrappers.molecule import Molecule, MolecularOrbitals
 
 
@@ -81,50 +79,6 @@ class MOTBasedTool(object):
     @doc_inherit(Molecule, 'numbers')
     def numbers(self):
         return self._numbers
-
-    @property
-    @doc_inherit(MolecularOrbitals, 'nelectrons')
-    def nelectrons(self):
-        return self._mo.nelectrons
-
-    @property
-    @doc_inherit(MolecularOrbitals, 'homo_index')
-    def homo_index(self):
-        return self._mo.homo_index
-
-    @property
-    @doc_inherit(MolecularOrbitals, 'lumo_index')
-    def lumo_index(self):
-        return self._mo.lumo_index
-
-    @property
-    @doc_inherit(MolecularOrbitals, 'homo_energy')
-    def homo_energy(self):
-        return self._mo.homo_energy
-
-    @property
-    @doc_inherit(MolecularOrbitals, 'lumo_energy')
-    def lumo_energy(self):
-        return self._mo.lumo_energy
-
-    @property
-    @doc_inherit(MolecularOrbitals, 'occupation')
-    def orbital_occupation(self):
-        return self._mo.occupation
-
-    @property
-    @doc_inherit(MolecularOrbitals, 'energy')
-    def orbital_energy(self):
-        return self._mo.energy
-
-    @property
-    @doc_inherit(MolecularOrbitals, 'coefficient')
-    def orbital_coefficient(self):
-        return self._mo.coefficient
-
-    @doc_inherit(Molecule, 'compute_molecular_orbital')
-    def compute_orbital_expression(self, points, spin='ab', index=None):
-        return self._molecule.compute_molecular_orbital(points, spin, index)
 
     def compute_charges(self, scheme="mulliken"):
         """Return the partial charges at each atom using the given population analysis method.
@@ -210,48 +164,48 @@ class MOTBasedTool(object):
 
         return bond_order
 
-    def generate_scripts(self, fname, spin='a', index=None, isosurf=0.05, grid=None):
-        """Generate VMD script(s) and cube file(s) to visualize MO iso-surface of given orbitals.
-
-        Parameters
-        ----------
-        fname : str
-            A string representing the path to a fname of generated files.
-            The VMD script and cube file will be named fname_mo{index}.vmd and
-            fname_mo{index}.cube, respectively.
-        spin : str, optional
-           The type of occupied spin orbitals. Choose either 'a' or 'b'.
-        index : int, optional
-           Integer representing the index of spin orbital to visualize. Spin orbitals are each
-           indexed from 1 to :attr:`nbasis`. If None, files for visualizing all orbitals are
-           generated.
-        isosurf : float, optional
-            Value of MO iso-surface used in VMD script.
-        grid : UniformGrid, optional
-           Instance of UniformGrid used for computation and generating cube file(s).
-           If None, a cubic grid is constructed from molecule with spacing=0.2 & extension=5.0.
-
-        """
-        if spin not in ['a', 'b']:
-            raise ValueError('Argument spin can only be "a" or "b".')
-        if index is not None and not isinstance(index, int):
-            raise ValueError('Argument index is either None or an integer for visualization. '
-                             'Given index={0}'.format(index))
-        if grid is None:
-            grid = UniformGrid.from_molecule(self._molecule, spacing=0.2, extension=5.0, rotate=True)
-        elif not isinstance(grid, UniformGrid):
-            raise ValueError('Argument grid should be a UniformGrid to generate cube files.')
-
-        if index is None:
-            spin_index = {'a': 0, 'b': 1}
-            index = range(1, self._mo.homo_index[spin_index[spin]] + 1)
-        else:
-            index = [index]
-
-        for mo_index in index:
-            vmdname = fname + '_mo{0}.vmd'.format(mo_index)
-            cubname = fname + '_mo{0}.cube'.format(mo_index)
-            mo_value = self.compute_orbital_expression(grid.points, spin=spin, index=mo_index)
-            grid.generate_cube(cubname, mo_value)
-            print_vmd_script_isosurface(vmdname, cubname, isosurf=isosurf, negative=True,
-                                        material='BlownGlass')
+    # def generate_scripts(self, fname, spin='a', index=None, isosurf=0.05, grid=None):
+    #     """Generate VMD script(s) and cube file(s) to visualize MO iso-surface of given orbitals.
+    #
+    #     Parameters
+    #     ----------
+    #     fname : str
+    #         A string representing the path to a fname of generated files.
+    #         The VMD script and cube file will be named fname_mo{index}.vmd and
+    #         fname_mo{index}.cube, respectively.
+    #     spin : str, optional
+    #        The type of occupied spin orbitals. Choose either 'a' or 'b'.
+    #     index : int, optional
+    #        Integer representing the index of spin orbital to visualize. Spin orbitals are each
+    #        indexed from 1 to :attr:`nbasis`. If None, files for visualizing all orbitals are
+    #        generated.
+    #     isosurf : float, optional
+    #         Value of MO iso-surface used in VMD script.
+    #     grid : UniformGrid, optional
+    #        Instance of UniformGrid used for computation and generating cube file(s).
+    #        If None, a cubic grid is constructed from molecule with spacing=0.2 & extension=5.0.
+    #
+    #     """
+    #     if spin not in ['a', 'b']:
+    #         raise ValueError('Argument spin can only be "a" or "b".')
+    #     if index is not None and not isinstance(index, int):
+    #         raise ValueError('Argument index is either None or an integer for visualization. '
+    #                          'Given index={0}'.format(index))
+    #     if grid is None:
+    #         grid = UniformGrid.from_molecule(self._molecule, spacing=0.2, extension=5.0, rotate=True)
+    #     elif not isinstance(grid, UniformGrid):
+    #         raise ValueError('Argument grid should be a UniformGrid to generate cube files.')
+    #
+    #     if index is None:
+    #         spin_index = {'a': 0, 'b': 1}
+    #         index = range(1, self._mo.homo_index[spin_index[spin]] + 1)
+    #     else:
+    #         index = [index]
+    #
+    #     for mo_index in index:
+    #         vmdname = fname + '_mo{0}.vmd'.format(mo_index)
+    #         cubname = fname + '_mo{0}.cube'.format(mo_index)
+    #         mo_value = self.compute_orbital_expression(grid.points, spin=spin, index=mo_index)
+    #         grid.generate_cube(cubname, mo_value)
+    #         print_vmd_script_isosurface(vmdname, cubname, isosurf=isosurf, negative=True,
+    #                                     material='BlownGlass')
