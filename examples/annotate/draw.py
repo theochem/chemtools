b1dc0a1c812531e2cd97b852d234cdb2b12d9d46
nwcad2020 <nwcadwebmaster2020@protonmail.com>
2021-07-21 23:54:50 -0400

    currently nonfunctioning dihedral angle calculator

--- examples/annotate/draw.py
+++ examples/annotate/draw.py
@@ -1,8 +1,10 @@
 import sys,os
 
 from rdkit import Chem
+from rdkit.Chem import AllChem 
+from rdkit.Chem import Draw
 from rdkit.Chem.Draw import rdMolDraw2D
-
+from rdkit.Chem import rdMolTransforms 
 
 mol = Chem.MolFromSmiles('Cl[C@H](F)NC\C=C\C')
 image = rdMolDraw2D.MolDraw2DCairo(500,400) 
@@ -10,14 +12,18 @@ image = rdMolDraw2D.MolDraw2DCairo(500,400)
 def visualizeChiralCenter(image):
     image.drawOptions().addStereoAnnotation = True
 
-#def visFormalCharge(mol,image): 
-
+def dihedralAngles(mol,image): 
+    # NONFUNCTIONAL  
+    """
+    for q in mol.GetBonds():
+        bonds = (q.GetBeginAtomIdx(),q.GetEndAtomIdx()) 
+    AllChem.Compute2DCoords(mol) 
+    isomerize = mol.GetConformer()
     
-
-
-
-
-
+    #TORSION
+    rdMolTransforms.GetAngleDeg(isomerize,bonds) 
+    # DIHEDRAL 
+    """
 def imageGenerate(image):
     image.drawOptions().addAtomIndices = True
     image.DrawMolecule(mol) 
@@ -28,4 +34,6 @@ if len(sys.argv) >= 2:
     if sys.argv[1] == '-c' or sys.argv[1] == '--chirality': 
         visualizeChiralCenter(image) 
 
+
+dihedralAngles(mol,image) 
 imageGenerate(image)
