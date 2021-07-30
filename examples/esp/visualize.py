9f3887d5759cc882df4c965c02ca4686fbf438f2
Nathan Wood <nathanwcad@gmail.com>
2021-07-29 22:56:37 -0400

    added material call, cannot visualize negatives, added installation link

--- examples/esp/visualize.py
+++ examples/esp/visualize.py
@@ -3,6 +3,15 @@ from chimerax.core import errors
  
 # END MODULES
 """
+INSTALLATION OF CHIMERAX 
+
+CHIMERAX IS CREATED BY THE UNIVERSITY OF CALIFORNIA - SAN FRANCISCO
+
+VERSIONS FOR DIFFERENT OPERATING SYSTEMS CAN BE FOUND HERE
+
+https://www.cgl.ucsf.edu/chimerax/download.html
+
+
 THIS SCRIPT FIRST OPENS ESP AND RHO CUBE FILES, VISUALIZES THE ISOSURFACES OF THE ESP CUBE FILE USING VOLUME VIEWER
 
 WE NEED TO FIND A MEANS OF IMPLEMENTING FSTRING{rhoName} etc.  
@@ -15,13 +24,13 @@ CURRENTLY THE INPUT *_esp.cube and *_rho.cube inputs, ISOSURFACE LEVELS, AND OUT
 
 """ 
 
-
 def VisualizeMolecule(session): 
 
     run(session, "open dichloropyridine26_q+0_esp.cube")
     run(session, "open dichloropyridine26_q+0_rho.cube")
-    run(session, "volume #1 style surface level .0555") 
+    run(session, "volume #1 style surface level .0675 ") 
     run(session, "lighting simple")
+    run(session, "material dull") 
     run(session, "graphics silhouettes false")
     run(session, "lighting shadows false")  
     run(session, "hide #2 model") 
