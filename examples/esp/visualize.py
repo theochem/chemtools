30e0c4a3e0e284bbdbea8578daad32a69eab7f94
Nathan Wood <nathanwcad@gmail.com>
2021-08-08 22:42:34 -0400

    still errors with ascribing correct values to atoms

--- examples/esp/visualize.py
+++ examples/esp/visualize.py
@@ -1,6 +1,5 @@
 from chimerax.core.commands import run
 from chimerax.core import errors
- 
 # END MODULES
 """
 INSTALLATION OF CHIMERAX 
@@ -23,17 +22,20 @@ TO EXECUTE THIS SCRIPT RUN
 CURRENTLY THE INPUT *_esp.cube and *_rho.cube inputs, ISOSURFACE LEVELS, AND OUTPUT FILENAMES ARE HARD CODED 
 
 """ 
-
+import chimerax.surface
 def VisualizeMolecule(session): 
-
+    from chimerax.map import Volume,VolumeSurface
+    
+    test = 'dichloropyridine26_q+0_rho.cube' 
     run(session, "open dichloropyridine26_q+0_esp.cube")
-    run(session, "open dichloropyridine26_q+0_rho.cube")
+    run(session, 'open dichloropyridine26_q+0_esp.cube')  
     run(session, "volume #1 style surface level .0675 ") 
     run(session, "lighting simple")
     run(session, "material dull") 
     run(session, "graphics silhouettes false")
     run(session, "lighting shadows false")  
     run(session, "hide #2 model") 
+
 # END VISUALIZEMOLECULE 
 
 def ColorElectrostaticChimera(session): 
@@ -46,7 +48,6 @@ def SaveMoleculeChimera(session):
     run(session, "hide #2 model") 
     run(session, "save outFile.cxs") #CHIMERAX SAVE STATE 
     run(session, "save outFile.png") # SAVE AS PNG IMAGE    
-
 
 #END SAVEMOLECULECHIMERA 
 VisualizeMolecule(session)
