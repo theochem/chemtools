bf78d0b158a344d2b7844c75a174b4b47535b6f1
vcanogil <vcanogil@gmail.com>
2021-08-17 14:14:19 -0400

    minor doc fixes

--- chemtools/wrappers/molecule.py
+++ chemtools/wrappers/molecule.py
@@ -304,42 +304,9 @@ class AtomicOrbitals:
 
     @property
     def center_index(self):
-        # FIXME: following code is pretty hacky. it will be used for the orbital partitioning code
-        # GOBasis object stores basis set to atom and angular momentum mapping
-        # by shell and not by contraction. So it needs to be converted
+        """Get orbital partitioning from atomic shell information"""
         shells = AtomicShells(self._mol.obasis.shells)
         return shells.ind_basis_center
-        # try:
-        #     ind_shell_atom = np.array(self._basis.shell_map)
-        #     ind_shell_orbtype = np.array(self._basis.shell_types)
-
-        #     def num_contr(orbtype):
-        #         """Return the number of contractions for the given orbital type.
-
-        #         Parameters
-        #         ----------
-        #         orbtype : int
-        #             Horton's orbital type scheme.
-        #             Positive number corresponds to cartesian type and negative number corresponds to
-        #             spherical types.
-
-        #         Returns
-        #         -------
-        #         num_contr : int
-        #             Number of contractions for the given orbital type.
-
-        #         """
-        #         if orbtype < 0:
-        #             return 1 + 2 * abs(orbtype)
-        #         return 1 + orbtype + sum(i * (i + 1) / 2 for i in range(1, orbtype + 1))
-
-        #     numcontr_per_shell = np.array([num_contr(i) for i in ind_shell_orbtype])
-        #     # Duplicate each shell information by the number of contractions in shell
-        #     ind_basis_center = np.repeat(ind_shell_atom, numcontr_per_shell)
-        #     # self._ind_basis_orbtype = np.repeat(ind_shell_orbtype, numcontr_per_shell)
-        # except AttributeError:
-        #     pass
-        # return ind_basis_center
 
     def compute_overlap(self):
         r"""Return overlap matrix :math:`\mathbf{S}` of atomic orbitals.
@@ -353,7 +320,6 @@ class AtomicOrbitals:
             return overlap_integral(self._basis, coord_type=self._coord_type)
         else:
             raise NotImplementedError("Mixed coordinate types are not supported yet.")
-
 
     def compute_orbitals(self, points, transform=None):
         """ Evaluate Orbitals
