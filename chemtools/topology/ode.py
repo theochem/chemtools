b287f0b084f12e1fa55b372f768fd575325e1c05
Ali-Tehrani <alirezatehrani24@gmail.com>
2023-03-17 17:23:48 -0400

    Add checking for NNA during ode solving

--- chemtools/topology/ode.py
+++ chemtools/topology/ode.py
@@ -3,7 +3,15 @@ from scipy.spatial.distance import cdist
 
 from scipy.integrate import solve_ivp
 
-__all__ = ["steepest_ascent_rk45", "gradient_path"]
+__all__ = ["find_basins_steepest_ascent_rk45", "gradient_path"]
+
+
+def delete_duplicate_pts(pts, eps):
+    dists = cdist(pts, pts)
+    dists[np.isclose(dists, 0)] = np.inf
+    i, j = np.where(dists <= eps)
+    indices_to_delete = np.unique(i)[1:]
+    return np.delete(pts, indices_to_delete, axis=0), indices_to_delete
 
 
 def _get_normalized_gradient_func(grad_func):
@@ -11,6 +19,10 @@ def _get_normalized_gradient_func(grad_func):
     def norm_grad_func(x):
         grad = grad_func(x)
         return grad / np.linalg.norm(grad, axis=1)[:, None]
+        # norm_grad = np.linalg.norm(grad, axis=1)
+        # grad = grad / norm_grad[:, None]
+        # grad[np.abs(norm_grad) < 1e-45, :] = 0.0
+        # return grad
     return norm_grad_func
 
 
@@ -73,9 +85,9 @@ def _RK45_step(pts, grad_func, step_size, grad0=None):
     return y_four, y_five
 
 
-def steepest_ascent_rk45(
+def find_basins_steepest_ascent_rk45(
     initial_pts, dens_func, grad_func, beta_spheres, maximas, ss_0=1e-7,
-    tol=1e-7, max_ss=0.25, maxiter=100, terminate_if_other_basin_found=False
+    tol=1e-7, max_ss=0.25, maxiter=2000, iter_nna=100, hess_func=None, terminate_if_other_basin_found=False
 ):
     r"""
     Solves the following problem ODE using Runge-Kutta of order 4(5) with adaptive step-size
@@ -106,6 +118,12 @@ def steepest_ascent_rk45(
         Tolerance for the adaptive step-size.
     maxiter: int, optional
         The maximum number of iterations of taking a step in the ODE solver.
+    iter_nna: int, optional
+        Iteration to start checking for non-nuclear attractors and for the step-size to be reduced by 10.
+    hess_func: callable(ndarray(N, 3)->ndarray(N, 3, 3)), optional
+        The Hessian of the electron density. If this is provided, then non-nuclear attractors will be found.
+        Adds a default Lebedev/angular grid of degree fifty and 0.1 a.u. to the `beta-spheres` if it is
+        provided.
     terminate_if_other_basin_found : bool
         If true, then if multiple basin values were found, then the ODE solver will exit.
         If false, then the ODE solver will run until all points enter one of the
@@ -113,9 +131,11 @@ def steepest_ascent_rk45(
 
     Returns
     -------
-    ndarray(N,):
-        Integer array that assigns each point to a basin/maxima/atom of the molecule.
+    ndarray(N,), ndarray(M, 3):
+        - Integer array that assigns each point to a basin/maxima/atom of the molecule.
         If value is negative one, then the point wasn't assigned to a basin.
+        - Array of 3D coordinates of the maximas. New potential maximas are found and updated here.
+          This is only returned if `hess_func` is provided.
     
     """
     norm_grad_func = _get_normalized_gradient_func(grad_func)
@@ -140,7 +160,7 @@ def steepest_ascent_rk45(
     niter = 0  # Number of iterations
     grad0 = norm_grad_func(pts)  # Avoids re-computing the gradient twice, used to check for NNA
     print("START STEEPEST-ASCENT")
-    # import time
+    import time
     while len(not_found_indices) != 0:
         if niter == maxiter:
             raise RuntimeError(
@@ -150,10 +170,10 @@ def steepest_ascent_rk45(
                 f"non-nuclear attractor to the list of critical points."
             )
 
-        #start = time.time()
+        start = time.time()
         y_four, y_five = _RK45_step(pts, norm_grad_func, ss, grad0)
-        # final = time.time()
-        # print("RK Step ", final - start)
+        final = time.time()
+        print(f"Number Iterations {niter} RK Step {final - start}   and number of points left  {len(not_found_indices)}")
 
         # Update step-size
         # print("Step size used", ss)
@@ -178,7 +198,7 @@ def steepest_ascent_rk45(
         beta_sph = dist_maxima <= beta_spheres
         which_basins = np.where(beta_sph)
         # print("beta_sphereS ", beta_spheres)
-        # print("which pts are within basin based on beta-sphere", which_basins)
+        #print("which pts are within basin based on beta-sphere", which_basins)
         # print("basins", which_basins[1])
         if len(which_basins[0]) != 0:
             assigned_basins[not_found_indices[which_basins[0]]] = which_basins[1]
@@ -201,41 +221,173 @@ def steepest_ascent_rk45(
                         return assigned_basins
         # print("New indices to still do: ", not_found_indices)
 
-        # Check for non-nuclear attractors (NNA)
+        # For the points that didn't converge, check for non-nuclear attractors (NNA)
         if len(y_five) != 0:
-            grad0 = norm_grad_func(y_five)
-            # Note `gradient` that this is an extra computation and could be removed
-            i_smallg = np.where(
-                (np.all(np.abs(grad0) < 1e-6, axis=1)) & (dens_vals1 > 0.001)
-            )[0]
-            if len(i_smallg) != 0:
-                #  0.001 a.u. obtained from the paper: An Efficient Grid-Based Scheme to Compute QTAIM
-                #  Atomic Properties without Explicit Calculation ofZero-Flux Surfaces
-                # Assign these points to basin -2,  delete them.
-                print(f"Maximas {maximas}")
-                print(f"Where the NNCP is {y_five[i_smallg]}")
-                print(f"Gradient {grad0[i_smallg]}")
-                print(f"Density {dens_vals1[i_smallg]}")
-                print(f"Distance to each maxima {cdist(y_five[i_smallg], maximas)}")
-                raise RuntimeError(
-                    f"Non-nuclear attractor was found! Exiting"
-                )
-                assigned_basins[not_found_indices[i_smallg]] = -2
-                not_found_indices = np.delete(not_found_indices, i_smallg)
-                y_five = np.delete(y_five, i_smallg, axis=0)
-                ss = np.delete(ss, i_smallg)[:, None]
-                dens_vals1 = np.delete(dens_vals1, i_smallg)
+            grad_vals = grad_func(y_five)
+            grad0 = grad_vals / np.linalg.norm(grad_vals, axis=1)[:, None]
+            # Check maxiter is relatively large, to avoid computing the Hessian multiple times.
+            if niter >= iter_nna:
+                i_smallg = np.where(
+                    (np.all(np.abs(grad_vals) < 1e-5, axis=1)) & (dens_vals1 > 0.001)
+                )[0]
+                # Decrease the step-size because the gradient doesn't get small enough
+                ss /= 10
+                if len(i_smallg) != 0:
+                    # if Hessian is provided, then check for NNA
+                    if hess_func is not None:
 
+                        hess = hess_func(y_five[i_smallg])
+                        eigs = np.linalg.eigvalsh(hess)
+                        print(eigs)
+                        # Check if local maxima:
+                        which_is_nna = np.where(np.all(eigs < -1e-10, axis=1))[0]
+                        if len(which_is_nna) != 0:
+                            nna_indices = i_smallg[which_is_nna]
+                            # Found a NNA, Remove Duplicates, Update maxima and beta-spheres
+                            new_maximas, indices_to_delete = delete_duplicate_pts(y_five[nna_indices], 1e-3)
+
+                            maximas = np.vstack((maximas, new_maximas))
+                            beta_spheres = np.append(beta_spheres, [0.01] * len(new_maximas))
+
+                            assigned_basins[not_found_indices[nna_indices]] = len(maximas) - 1
+                            not_found_indices = np.delete(not_found_indices, nna_indices)
+                            y_five = np.delete(y_five, nna_indices, axis=0)
+                            ss = np.delete(ss, nna_indices)[:, None]
+                            dens_vals1 = np.delete(dens_vals1, nna_indices)
+                            grad0 = np.delete(grad0, nna_indices, axis=0)
+                    else:
+                        # Assign these points to basin -2,  delete them.
+                        print(f"Maximas {maximas}")
+                        print(f"Where the NNCP is {y_five[i_smallg]}")
+                        print(f"Gradient {grad_vals[i_smallg]}")
+                        print(f"Density {dens_vals1[i_smallg]}")
+                        print(f"Distance to each maxima {cdist(y_five[i_smallg], maximas)}")
+                        raise RuntimeError(
+                            f"Non-nuclear attractor was found! Exiting"
+                        )
 
         # Update next iteration
         pts = y_five.copy()
         dens_vals0 = dens_vals1
         niter += 1
-        # print("pts", pts)
 
         # input("Next step")
     # print("Final basins ", assigned_basins)
+    if hess_func is not None:
+        return assigned_basins, maximas
     return assigned_basins
+
+
+def steepest_ascent_rk45(
+        initial_pts, dens_func, grad_func, ss_0=1e-7, tol=1e-7, max_ss=0.25, tol_conv=1e-10, maxiter=2000
+):
+    r"""
+    Solves the following problem ODE using Runge-Kutta of order 4(5) with adaptive step-size
+
+    .. math::
+        \frac{d(r(t))}{dt} = \frac{\nabla \rho(r(t))}{|| \rho(r() ||}
+
+    over a set of points.
+
+    Parameters
+    ----------
+    initial_pts: ndarray(N, 3)
+        Initial points to solve for steepest-ascent/backtracing.
+    dens_func: callable(ndarray(N,3), ndarray(N))
+        The electron density function.
+    grad_func: callable(ndarray(N,3), ndarray(N,3))
+        The gradient of the electron density.
+    beta_spheres: ndarray(M,)
+        The beta-sphere/trust-region radius of each atom. These are spheres
+        centered at each maxima that reduces the convergence of each point.
+    maximas: ndarray(M,3)
+        The position of each atoms in the molecule.
+    ss_0: float, optional
+        The initial step-size of the ODE (RK45) solver.
+    max_ss: float, optional
+        Maximum step-size of the ODE (RK45) solver.
+    tol_conv: float, optional
+        Tolerance for the convergence of a point from a ODE step.
+    tol: float, optional
+        Tolerance for the adaptive step-size.
+    maxiter: int, optional
+        The maximum number of iterations of taking a step in the ODE solver.
+    terminate_if_other_basin_found : bool
+        If true, then if multiple basin values were found, then the ODE solver will exit.
+        If false, then the ODE solver will run until all points enter one of the
+        beta-sphere/trust-region.
+
+    Returns
+    -------
+    ndarray(N,):
+        Integer array that assigns each point to a basin/maxima/atom of the molecule.
+        If value is negative one, then the point wasn't assigned to a basin.
+
+    """
+    norm_grad_func = _get_normalized_gradient_func(grad_func)
+
+    numb_pts = initial_pts.shape[0]
+    if isinstance(ss_0, float):
+        ss = np.ones((numb_pts, 1)) * ss_0
+    elif isinstance(ss_0, np.ndarray):
+        if not ss_0.shape[1] == 1:
+            raise ValueError(f"Steps-size {ss_0.shape} should have shape of the form (N, 1).")
+        ss = ss_0.copy()
+    else:
+        raise TypeError(f"Step-size ss_0 {type(ss_0)} should have type float or array.")
+
+    pts = initial_pts.copy()
+    dens_vals0 = dens_func(initial_pts)
+
+    not_found_indices = np.arange(numb_pts)
+    niter = 0  # Number of iterations
+    grad0 = norm_grad_func(pts)  # Avoids re-computing the gradient twice, used to check for NNA
+    pts_curr = pts.copy()
+    final_pts = []
+    while len(not_found_indices) != 0:
+        if niter == maxiter:
+            raise RuntimeError(
+                f"Number of iterations reached maximum {niter}, "
+                f"this may be because of a non-nuclear attractor (NNA) which may cause the ODE "
+                f"to cycle between two points. Repeat this calculation by including the "
+                f"non-nuclear attractor to the list of critical points."
+            )
+        y_four, y_five = _RK45_step(pts_curr, norm_grad_func, ss, grad0)
+
+        # Update step-size
+        # print("Step size used", ss)
+        ss = (tol * ss / (2.0 * np.linalg.norm(y_five - y_four, axis=1)[:, None])) ** 0.25
+        ss[ss > max_ss] = max_ss
+
+        # Get density values and if the density-values decreased, reduce step-size
+        dens_vals1 = dens_func(y_five)
+        indices = np.where(dens_vals1 <= dens_vals0)[0]
+        # print("New density values ", dens_vals1)
+        # print("Indices that density decreaed ", indices)#, dens_vals1[indices], dens_vals0[indices])
+        if len(indices) != 0:
+            # print("Density Decreased")
+            # print("Gradients here", grad_func(pts[indices, :]))
+            y_five[indices, :] = pts_curr[indices, :]
+            ss[indices] *= 0.25
+
+        # Check any points are within the beta-spheres and remove them if they converged.
+        converged = np.where(np.all(np.abs(y_five - pts_curr) < tol_conv, axis=1))[0]
+        if len(converged) != 0:
+            not_found_indices = np.delete(not_found_indices, converged)
+            final_pts.append(y_five[converged])
+            y_five = np.delete(y_five, converged, axis=0)
+            ss = np.delete(ss, converged)[:, None]
+            dens_vals1 = np.delete(dens_vals1, converged)
+
+        # Update next iteration
+        grad0 = norm_grad_func(y_five)
+        pts_curr = y_five.copy()
+        dens_vals0 = dens_vals1
+        niter += 1
+
+        # input("Next step")
+    # print("Final basins ", assigned_basins)
+    return np.vstack(final_pts)
 
 
 def gradient_path(pt, grad_func, use_norm=False, maximas=None, t_span=(0, 1000), method="LSODA", max_step=100,
@@ -300,7 +452,7 @@ def gradient_path(pt, grad_func, use_norm=False, maximas=None, t_span=(0, 1000),
             if convergence < 1e-1:
                 return sol["y"][:, -1]
         # No convergence occured, so increaes t-span.
-        # print(sol["y"][:, -1], t_span, "YE")
+        print(sol["y"][:, -1], t_span, "YE")
         t_span = (t_span[1], t_span[1] + t_inc)
         y0 = sol["y"][:, -1]
         numb_times += 1
