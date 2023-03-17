dc9c6321b190abb3036e9dcea56df7c1b9a2e894
Ali-Tehrani <alirezatehrani24@gmail.com>
2023-03-17 17:22:48 -0400

    Add finding NNA during beta-spheres

--- chemtools/topology/utils.py
+++ chemtools/topology/utils.py
@@ -1,8 +1,9 @@
+from grid.cubic import UniformGrid
 import numpy as np
 from scipy.spatial.distance import cdist
 from scipy.optimize import root_scalar
 
-from chemtools.topology.ode import steepest_ascent_rk45, gradient_path
+from chemtools.topology.ode import find_basins_steepest_ascent_rk45, steepest_ascent_rk45, gradient_path
 
 r"""
 Utility functions that is common between the QTAIM algorithms.
@@ -12,7 +13,7 @@ __all__ = [
     "solve_for_oas_points",
     "construct_radial_grids",
     "find_optimize_centers",
-    "determine_beta_spheres",
+    "determine_beta_spheres_and_nna",
     "solve_for_isosurface_pt"
 ]
 
@@ -81,15 +82,17 @@ def find_optimize_centers(centers, grad_func):
     return maximas
 
 
-def construct_radial_grids(maximas, min=0.2, pad=5.0, ss0=0.23):
+def construct_radial_grids(pts1, maximas, min_pt=0.2, pad=5.0, ss0=0.23):
     r"""
-    Construct radial grids
+    Construct radial grids around each maxima depending on its neighbors
 
     Parameters
     ----------
+    pts: ndarray(M_1, 3)
+        Coordinates of the points to construct radial grids on.
     maximas: ndarray(M, 3)
         Coordinates of the maximas
-    min: float
+    min_pt: float
         The minimum radial value on [0, \infty) to construct a radial grid.
     pad: float
         Extra padding to add to make sure the radial grid covers the intersection
@@ -100,7 +103,7 @@ def construct_radial_grids(maximas, min=0.2, pad=5.0, ss0=0.23):
     Returns
     -------
     list[ndarray]:
-        List of radial grid of length number of maximas. The radial grid are uniform
+        List of radial grid of length `M_1`. The radial grid are uniform
         grids that start at `min` and end on the maximum distance to the fifth atom plus
         an extra padding with stepsize `ss0`.
 
@@ -109,16 +112,19 @@ def construct_radial_grids(maximas, min=0.2, pad=5.0, ss0=0.23):
     #  Added an extra padding in the case of carbon in CH4
     #  TODO: the upper-bound should depend on distance to isosurface value and distance
     #         between atoms
-    dist_maxs = cdist(maximas, maximas)
-    distance_maximas = np.sort(dist_maxs, axis=1)[:, min(5, maximas.shape[0] - 1)]
-    print(cdist(maximas, maximas))
+    dist_maxs = cdist(pts1, maximas)
+    sorted_dists = np.sort(dist_maxs, axis=1)
+    distance_maximas = sorted_dists[:, min(5, maximas.shape[0] - 1)]
+    distance_minimas = sorted_dists[:, 1] / 4.0
     radial_grid = [
-        np.arange(min, x + pad, ss0) for x in distance_maximas
+        np.arange(min(min_pt, distance_minimas[i]), x + pad, ss0) for i, x in enumerate(distance_maximas)
     ]
     return radial_grid
 
 
-def determine_beta_spheres(beta_spheres, maximas, radial_grid, angular_pts, dens_func, grad_func):
+def determine_beta_spheres_and_nna(
+        beta_spheres, maximas, radial_grids, angular_pts, dens_func, grad_func, hess_func=None
+):
     r"""
 
     Notes this assumes the initial beta-sphere is 0.01, and so the distance between maximas
@@ -132,18 +138,33 @@ def determine_beta_spheres(beta_spheres, maximas, radial_grid, angular_pts, dens
     # TODO: add the sphere angle trick.
 
     # Determine the beta-spheres
-    for i_maxima, maxima in enumerate(maximas):
+    i_maxima = 0
+    while i_maxima != len(maximas):
+        maxima = maximas[i_maxima]
         if beta_spheres[i_maxima] == initial_beta_sph:
-            optimal_rad = -np.inf
-            for rad_pt in radial_grid[i_maxima]:
+            for rad_pt in radial_grids[i_maxima]:
                 if rad_pt > initial_beta_sph:
                     # Determine the points on the sphere with this radius
                     pts = maxima + rad_pt * angular_pts
-                    basins = steepest_ascent_rk45(
-                       # pts, dens_func, grad_func, beta_spheres, maximas, tol=1e-9, max_ss=0.1, ss_0=0.05
-                        pts, dens_func, grad_func, beta_spheres, maximas, ss_0=0.23, max_ss=0.5, tol=1e-7,
+                    # You want here for the ODE to be accurate in-order to find potential NNA.
+                    basins, maximas = find_basins_steepest_ascent_rk45(
+                        pts, dens_func, grad_func, beta_spheres, maximas, ss_0=0.2, max_ss=0.2, tol=1e-9,
+                        hess_func=hess_func
                     )
                     basins = np.array(basins, dtype=np.int)
+
+                    which_nna = np.where(basins >= numb_maximas)[0]
+                    if len(which_nna) != 0:
+                        # Copy a radial grid from the previous method
+                        radial_grids += \
+                            construct_radial_grids(maximas[numb_maximas:], maximas[:numb_maximas], 0.1, 5.0, 0.2)
+
+                        print(maximas)
+                        print(beta_spheres)
+                        beta_spheres += [initial_beta_sph] * (len(maximas) - numb_maximas)
+                        numb_maximas = len(maximas)
+                        #input("Found NNA")
+
                     # If all the basins went to same maxima, then update radius
                     # else then break out of this for loop.
                     if np.all(basins == i_maxima):
@@ -153,9 +174,10 @@ def determine_beta_spheres(beta_spheres, maximas, radial_grid, angular_pts, dens
                         print("Optimal radius is ", optimal_rad)
                     else:
                         break
-        print(f"i Maxima {i_maxima} and Final optimal radius {optimal_rad}")
+        print(f"i Maxima {i_maxima} and Final optimal radius {beta_spheres[i_maxima]}")
+        i_maxima += 1
         # input("next maxima")
-    return beta_spheres
+    return beta_spheres, maximas, radial_grids
 
 
 def solve_for_isosurface_pt(
@@ -222,3 +244,58 @@ def solve_for_isosurface_pt(
     assert sol.converged, f"Root function did not converge {sol}."
     bnd_pt = maxima + sol.root * cart_sphere_pt
     return bnd_pt
+
+
+def find_non_nuclear_attractors(maximas, dens_func, grad_func, hess_func):
+    r"""
+    Finds non-nuclear attractors up to two decimal places.
+
+    Returns
+    -------
+    ndarray(M+K, 3)
+        Returns the original `M` maximas from `maximas` and adds `K` new maximas that are
+        non-nuclear attractors.
+
+    """
+    grid = UniformGrid.from_molecule(
+        [6.0] * len(maximas), maximas, spacing=0.1, rotate=False, extension=2.0,
+    )
+
+    dens_vals = dens_func(grid.points)
+    indices2 = np.where(dens_vals > 0.001)[0]
+    grads_vals = grad_func(grid.points[indices2])
+    print(grads_vals)
+    indices = np.where(np.all(np.abs(grads_vals) < 0.01, axis=1))[0]
+    print(indices2)
+    print(indices)
+    if len(indices) != 0:
+        np.set_printoptions(threshold=np.inf)
+        pts = steepest_ascent_rk45(
+            grid.points[indices2][indices], dens_func, grad_func, tol_conv=1e-8
+        )
+        print(pts)
+        dist_maxima = cdist(pts, maximas)
+        print(dist_maxima)
+        beta_sph = dist_maxima <= [0.1] * len(maximas)
+        which_basins = np.where(beta_sph)
+
+        pts = np.delete(pts, which_basins[0], axis=0)
+        print("Pts ")
+        if len(pts) != 0:
+            pts = np.unique(np.round(pts, decimals=1), axis=0)
+            # print(pts)
+            nna_attractors = np.array(
+                [gradient_path(x, grad_func, t_span=(0, 30), method="BDF",
+                               first_step=1e-9, max_step=1e-1) for x in pts],
+                dtype=np.float64
+            )
+            nna_attractors = np.unique(np.round(nna_attractors, 2), axis=0)
+            print(nna_attractors)
+            eigs = np.linalg.eigvalsh(hess_func(nna_attractors))
+            print("eigs", eigs)
+            which_is_nna = np.where(np.all(eigs < -1e-10, axis=1))[0]
+            print(which_is_nna)
+
+            maximas = np.vstack((maximas, nna_attractors[which_is_nna]))
+            print(maximas)
+    return maximas
